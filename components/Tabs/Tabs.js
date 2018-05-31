c264ab84becf7fcb7c93cb2ced45d6f5b6fd8e1f
Wdifulvio523 <w.difulvio523@gmail.com>
2018-05-31 16:01:28 -0400

    Created reference to .tabs classes, and mapped through each tabs element.

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -83,7 +83,7 @@ class TabsItem {
 
 
 // START HERE: create a reference to the ".tabs" classes
-let tabs = document.querySelectorAll();
+let tabs = document.querySelectorAll(".tabs");
 // map through each tabs element and create a new Tabs object.  Be sure to pass in a reference to the tab when creating the Tabs object.
-tabs = Array.from(tabs).map();
+tabs = Array.from(tabs).map(tab => new Tabs(tab));
 
