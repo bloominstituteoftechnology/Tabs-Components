e5e397e1a973fc535cb0281adaf1f7b34ae6d9b8
Blake Fletcher <blakefletcher@Blakes-MacBook-Air.local>
2018-05-31 16:36:01 -0400

    Second commit
    Fixed minor syntactical errors
    Added classList
    Completed Dropdown.js

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -5,16 +5,17 @@ class Dropdown {
     // assign the ".dropdown-button" class found in the dropdown element (look at the HTML for context)
     this.button = this.element.querySelector('.dropdown-button');
     // assign the reference to the ".dropdown-content" class found in the dropdown element
-    this.content = this.element.querySelector('dropdown-content');
+    this.content = this.element.querySelector('.dropdown-content');
     // Add a click handler to the button reference and call the toggleContent method.
-    this.button.addEventListener("click", () => {
-        this.toggleContent();// Add a click handler to the button reference and call the toggleContent method.
-      });
+    this.button.addEventListener('click', () => {
+      this.toggleContent();
+    // Add a click handler to the button reference and call the toggleContent method.
+    });
   }
 
   toggleContent() {
     // Toggle the ".dropdown-hidden" class off and on
-    this.content.toggle('dropdown-hidden');
+    this.content.classList.toggle('dropdown-hidden'); // ADDED CLASSLIST AND MADE A HUGE DIFFERENCE
   }
 }
 // Nothing to do here, just study what the code is doing and move on to the Dropdown class
