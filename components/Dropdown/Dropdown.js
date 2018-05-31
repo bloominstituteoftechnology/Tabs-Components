14a6a8beecaa0477badb181a17f15d95a96a9b65
chainchompa <chainchompa@gmail.com>
2018-05-31 15:25:04 -0400

    completed dropdown menu

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -1,20 +1,18 @@
 class Dropdown {
   constructor(element) {
-    // assign this.element to the dropdown element
-    this.element;
-    // assign the ".dropdown-button" class found in the dropdown element (look at the HTML for context)
-    this.button;
-    // assign the reference to the ".dropdown-content" class found in the dropdown element
-    this.content;
-    // Add a click handler to the button reference and call the toggleContent method.
-    
+    this.element = dropdowns;
+    this.button = dropdownbtn;
+    this.content = dropdownContent;
+    this.button.addEventListener("click", () => {
+      this.toggleContent();
+    })
   }
 
   toggleContent() {
-    // Toggle the ".dropdown-hidden" class off and on
-    this.content;
+    this.content.classList.toggle("dropdown-hidden");
   }
 }
-// Nothing to do here, just study what the code is doing and move on to the Dropdown class
+let dropdownContent = document.querySelector('.dropdown-content')
+let dropdownbtn = document.querySelector('.dropdown-button')
 let dropdowns = document.querySelectorAll('.dropdown');
 dropdowns = Array.from(dropdowns).map( dropdown => new Dropdown(dropdown));
