2c38ed9247f0d4a35edae30f98f2798f3d33c575
TangledTessellations <mcevoya@oregonstate.edu>
2018-05-31 14:00:12 -0700

    Completed tab goals

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -3,6 +3,7 @@ class Tabs {
     this.element = element;
     // create a reference to the ".tabs-link" class nested in your tab object
     this.links = this.element.querySelectorAll(".tabs-link");
+    console.log(this.links);
     // This step will map over the array creating new TabsLink class instances of each link.  No need to update anything here, just study what is going on.  Notice that we are creating another new object using the TabsLink class.
     this.links = Array.from(this.links).map( link => {
       return new TabsLink(link, this);
@@ -27,7 +28,7 @@ class Tabs {
 
   getTab(data) {
     // return a reference to the element's data attribute
-    return this.element.querySelector(`.tabs-item[data-tab="${}"]`);
+    return this.element.querySelector(`.tabs-item[data-tab="${data}"]`);
   }
 
 }
@@ -41,24 +42,24 @@ class TabsLink {
     // Using the method from the parent class above, pass in a reference to the custom data attribute.
     this.tabsItem = parent.getTab(this.element.dataset.tab);
     // Create a new TabsItem object that passes in a tabsItem value that you just created above
-    this.tabsItem;
+    this.tabsItem = new TabsItem(this.tabsItem);
     this.element.addEventListener('click', () => {
       this.tabs.updateActive(this);
       // invoke the select() method on this
-      this;
+      this.select();
     });
   };
 
   select() {
     // Add a class named "tabs-link-selected" to the element
-    this.element;
+    this.element.classList.add("tabs-link-selected");
     // Notice that we are using the select method on tabsItem
     this.tabsItem.select();
   }
 
   deselect() {
     // Remove a class named "tabs-link-selected" from the element
-    this.element;
+    this.element.classList.remove("tabs-link-selected");
     // Notice that we are using the deselect method on tabsItem
     this.tabsItem.deselect();
   }
