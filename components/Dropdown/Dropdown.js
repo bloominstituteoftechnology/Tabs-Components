ca243ec519639d8e05ad5842a0b93bb37a3a24fc
DirupT <iamcooled@gmail.com>
2018-05-31 17:35:15 -0500

    added some animations

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -18,3 +18,4 @@ class Dropdown {
 // Nothing to do here, just study what the code is doing and move on to the Dropdown class
 let dropdowns = document.querySelectorAll('.dropdown');
 dropdowns = Array.from(dropdowns).map( dropdown => new Dropdown(dropdown));
+
