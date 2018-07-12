6c215d022e7f958a56f4c9215a22c260b11e6edc
szincone <sawyerzincone@gmail.com>
2018-07-12 17:47:33 -0600

    Finished Main

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -20,24 +20,3 @@ class Dropdown {
 let dropdowns = document.querySelectorAll('.dropdown');
 dropdowns = Array.from(dropdowns).map( dropdown => new Dropdown(dropdown));
 
-
-
-
-
-//// Sample Code from yesterday
-// const toggleMenu = () => {
-//   // Toggle the "menu--open" class on your menu refence.
-//   menu.classList.toggle("menu--open");
-// }
-
-// // Start Here: Create a reference to the ".menu" class
-// const menu = document.querySelector(".menu");
-
-// // create a reference to the ".menu-button" class
-// const menuButton = document.querySelector(".menu-button");
-
-
-// // Using your menuButton reference, add a click handler that calls toggleMenu
-// menuButton.addEventListener("click", (event) => {
-//   toggleMenu(event);
-// })
