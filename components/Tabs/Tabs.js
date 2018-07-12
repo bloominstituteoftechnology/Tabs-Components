decbe4c0245476b2afd383a86fcef1a6eedafbfc
luis salazar <luissalazar@luiss-MacBook-Pro.local>
2018-07-12 14:14:39 -0700

    Luis F. Salazar
    Major changes - dropmenu working.
    
    Started working on the Tabs.js file.

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -2,13 +2,15 @@ class Tabs {
   constructor(element) {
     this.element = element;
     // create a reference to the ".tabs-link" class nested in your tab object
-    this.links;
+    this.links = document.querySelector(".tabs-links");
     // This step will map over the array creating new TabsLink class instances of each link.  No need to update anything here, just study what is going on.  Notice that we are creating another new object using the TabsLink class.
     this.links = Array.from(this.links).map( link => {
       return new TabsLink(link, this);
     });
+    console.log(this.links);
     // Set the active link to the first item in the array
-    this.activeLink;
+    this.activeLink[0];
+    console.log(activeLink);
     // Nothing to update here, just notice we are invoking the init() method
     this.init();
   }
@@ -83,7 +85,8 @@ class TabsItem {
 
 
 // START HERE: create a reference to the ".tabs" classes
-let tabs = document.querySelectorAll();
+let tabs = document.querySelectorAll(".tabs");
+// console.log(tabs);
 // map through each tabs element and create a new Tabs object.  Be sure to pass in a reference to the tab when creating the Tabs object.
-tabs = Array.from(tabs).map();
+tabs = Array.from(tabs).map( tab => new Tabs(tab));
 
