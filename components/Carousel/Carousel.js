7c1f57ed8bf1582ff9c0f777083e27f09e721d5e
Grant Reighard <grantreighard@Grants-iMac.local>
2018-07-12 19:13:10 -0400

    Still working on carousel.

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -9,34 +9,34 @@ class CarouselImage {
         this.nextButton.addEventListener("click", this.scrollToNext);
         this.prevButton.addEventListener("click", this.scrollToLast);
         if (this.element.dataset.img === 0) {
-            this.prevImg = images.length - 1;
+            this.prevImg = images[images.length - 1];
         } else {
-            this.prevImg = this.element.dataset.img - 1;
+            this.prevImg = images[this.element.dataset.img - 1];
         }
         if (this.element.dataset.img === images.length - 1) {
             this.nextImg = images[0];
         } else {
-            this.nextImg = this.element.dataset.img + 1;
+            this.nextImg = images[this.element.dataset.img + 1];
         }
     }
 
     scrollToNext() {
-        this.nextImg.classList.toggle("carousel-item-show")
-        TweenMax.from(this.activeImg, 1, {x:0});
-        TweenMax.to(this.activeImg, 1, {x:-600});
-        TweenMax.from(this.nextImg, 1, {x:600});
+        // nextImg.classList.toggle("carousel-item-show");
+        TweenMax.from(activeImg, 1, {x:0});
+        TweenMax.to(activeImg, 1, {x:-800});
+        TweenMax.from(this.nextImg, 1, {x:800});
         TweenMax.to(this.nextImg, 1, {x:0});
         activeImg.classList.toggle("carousel-item-show");
-        activeImg = this.nextImg;
+        activeImg = nextImg;
         activeImg.classList.toggle("carousel-item-show");
 
     }
 
     scrollToLast() {
         TweenMax.from(activeImg, 1, {x:0});
-        TweenMax.to(activeImg, 1, {x:600});
-        TweenMax.from(prevImg, 1, {x:-600});
-        TweenMax.to(prevImg, 1, {x:0});
+        TweenMax.to(activeImg, 1, {x:800});
+        TweenMax.from(this.prevImg, 1, {x:-800});
+        TweenMax.to(this.prevImg, 1, {x:0});
         activeImg.classList.toggle("carousel-item-show");
         activeImg = prevImg;
         activeImg.classList.toggle("carousel-item-show");
