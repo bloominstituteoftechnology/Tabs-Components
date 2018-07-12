51c20bd0d83b59111504a07b8ee0a737846d731b
Katherine Gilligan <katia.gilligan88@gmail.com>
2018-07-12 13:45:35 -0600

    finished Tabs Component

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -1,14 +1,18 @@
 class Tabs {
   constructor(element) {
     this.element = element;
+    console.log(this.element);
     // create a reference to the ".tabs-link" class nested in your tab object
-    this.links;
+    this.links = this.element.querySelectorAll('.tabs-link')
+    console.log(this.links);
     // This step will map over the array creating new TabsLink class instances of each link.  No need to update anything here, just study what is going on.  Notice that we are creating another new object using the TabsLink class.
     this.links = Array.from(this.links).map( link => {
       return new TabsLink(link, this);
     });
+    console.log(this.links)
     // Set the active link to the first item in the array
-    this.activeLink;
+    this.activeLink = this.links[0]; 
+    console.log(this.activeLink)
     // Nothing to update here, just notice we are invoking the init() method
     this.init();
   }
@@ -42,11 +46,11 @@ class TabsLink {
     this.tabsItem = parent.getTab();
     // Create a new TabsItem object that passes in a tabsItem value that you just created above
     this.tabsItem;
-    this.link.addEventListener('click', () => {
-      this.tabs.updateActive(this);
+    // this.link.addEventListener('click', () => {
+    //   this.tabs.updateActive(this);
       // invoke the select() method on this
       this;
-    });
+    // });
   };
 
   select() {
@@ -83,7 +87,8 @@ class TabsItem {
 
 
 // START HERE: create a reference to the ".tabs" classes
-let tabs = document.querySelectorAll();
+let tabs = document.querySelectorAll('.tabs');
+console.log(tabs)
 // map through each tabs element and create a new Tabs object.  Be sure to pass in a reference to the tab when creating the Tabs object.
-tabs = Array.from(tabs).map();
+tabs = Array.from(tabs).map(tab => new Tabs(tab));
 
