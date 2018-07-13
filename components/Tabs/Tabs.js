65dd9b94b4912099d0dc9ec88125163dece74322
tulf <timzulf@gmail.com>
2018-07-12 20:28:49 -0400

    finally finished debugging. Need to understand why use element vs link. this.element vs document.

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -2,7 +2,7 @@ class Tabs {
   constructor(element) {
     this.element = element;
     // create a reference to the ".tabs-link" class nested in your tab object
-    this.links = document.querySelector('.tabs-link');
+    this.links = this.element.querySelectorAll('.tabs-link');
     // This step will map over the array creating new TabsLink class instances of each link.  No need to update anything here, just study what is going on.  Notice that we are creating another new object using the TabsLink class.
     this.links = Array.from(this.links).map( link => {
       return new TabsLink(link, this);
@@ -27,38 +27,38 @@ class Tabs {
 
   getTab(data) {
     // return a reference to the element's data attribute
-    return this.element.querySelector(`.tabs-item[data-tab"${this.data}"]`);
+    return this.element.querySelector(`.tabs-item[data-tab="${data}"]`)
   }
 
 }
 
 class TabsLink {
-  // notice that we passed in the link reference as well as a reference to the parent class.
+    // notice that we passed in the link reference as well as a reference to the parent class.
   constructor(link, parent) {
     this.link = link;
     // assign this.tabs to parent
     this.tabs = parent;
-    // Using the method from the parent class above, pass in a reference to the custom data attribute.
+     // Using the method from the parent class above, pass in a reference to the custom data attribute.
     this.tabsItem = parent.getTab(this.link.dataset.tab) //tabs item;
     // Create a new TabsItem object that passes in a tabsItem value that you just created above
-    this.tabsItem;
+    this.tabsItem = new TabsItem(this.tabsItem);
     this.link.addEventListener('click', () => {
-      this.tabs.updateActive(this);
       // invoke the select() method on this
+      this.tabs.updateActive(this);
       this.select();
     });
   };
 
   select() {
     // Add a class named "tabs-link-selected" to the link
-    this.element.classList.add("tabs-item-selected");
+    this.link.classList.add("tabs-item-selected");
     // Notice that we are using the select method on tabsItem
     this.tabsItem.select();
   }
 
   deselect() {
     // Remove a class named "tabs-link-selected" from the link
-    this.element.classList.remove("tabs-item-selected");
+    this.link.classList.remove("tabs-item-selected");
     // Notice that we are using the deselect method on tabsItem
     this.tabsItem.deselect();
   }
@@ -80,7 +80,6 @@ class TabsItem {
     // Congrats, you finished all the instruction, check out your tab navigator!
   }
 }
-
 
 // START HERE: create a reference to the ".tabs" classes
 let tabs = document.querySelectorAll(".tabs");
