180209e523fefd656e5d31fc2750c97afe647a4e
decagondev <decagondevelopment@gmail.com>
2018-08-16 20:29:32 +0100

    fixed typo .tabs-items to .tab-item

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -3,14 +3,17 @@ class TabLink {
   constructor(linkItem) {
     this.element = linkItem;
     console.log("element");
-    console.log(this.element);
+    console.log(this.element); // working as expected
     
     // Get the custom data attribute on the Link
-    this.data = this.element.dataset.tab; // getting an undefined here?!
+    this.data = this.element.dataset.tab; // working as expected
     console.log("dataset");
     console.log(this.data);
     // Using the custom data attribute get the associated Item element
-    this.itemElement = document.querySelector(`.tabs-items[data-tab="${this.data}"]`);
+    console.log("SELECTOR" + `.tabs-item[data-tab="${this.data}"]`); // selector looks correct
+    console.log("doc selector: " + document.querySelector(`.tabs-item[data-tab="${this.data}"]`)); // fixed typo .tabs-items to .tab-item
+    this.itemElement = document.querySelector(`.tabs-item[data-tab="${this.data}"]`);
+    console.log("ELEMS: " + this.itemElement);
     // Using the Item element, create a new instance of the TabItem class
     this.tabItem = new TabItem(this.tabItem);
     
@@ -57,7 +60,7 @@ class TabItem {
 
 
 // START HERE: create a reference to the ".tabs" classes
-let links = document.querySelectorAll(".tabs-link");
+let links = document.querySelectorAll(".tabs-link"); // spent over an hour debugging cos of this typo
 
 // Following the code in the Dropdown file, iterate through the array you created above creating a new instance of the TabLink class for each item. 
 links = Array.from(links).map(link => { return new TabLink(link); });
