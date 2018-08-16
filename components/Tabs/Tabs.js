88cbc9eafd337cc866ad23ed29ec8afae7b8738e
Emily McClanahan <emily.f.mcclanahan@gmail.com>
2018-08-16 17:55:59 -0500

    corrections to tabs.js, created proper functionality

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -20,11 +20,11 @@ class TabLink {
 
   select() {
     // Get all of the elements with the tabs-link class
-    const links = document.querySelector(`.tabs-link[data-tab='${this.data}']`);
+    const links = document.querySelectorAll(`.tabs-link`);
 
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-    Array.from(links).forEach(function() {
-      links.classList.remove('tabs-link-selected')
+    Array.from(links).forEach(function(link) {
+      link.classList.remove('tabs-link-selected')
     });
 
     // Add a class named "tabs-link-selected" to this link
@@ -41,11 +41,15 @@ class TabItem {
   }
 
   select() {
-    // Select all items elements from the DOM
-    this.itemElement.classList.toggle('tabs-item-selected')
+    // Select all tab items elements from the DOM
+    const items = document.querySelectorAll('.tabs-item')
     // Remove the class "tabs-item-selected" from each element
-   
+   items.forEach(function(item){
+     item.classList.remove('tabs-item-selected')
+
+   });
     // Add a class named "tabs-item-selected" to this element 
+    this.itemElement.classList.add('tabs-item-selected');
   }
 }
 
@@ -57,3 +61,4 @@ let links = document.querySelectorAll('.tabs-link');
 links = Array.from(links).map(link => new TabLink(link));
 
 // DO THIS LAST: Once you have created an array of TabLink instances. call select() on the first item in the array
+links[0].select();
