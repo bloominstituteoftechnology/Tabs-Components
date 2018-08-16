aa9165ffc0bf5e665a30fd3c7f518d041a81f4d9
Michael Agard <mdagard@gmail.com>
2018-08-16 15:36:18 -0400

    fixed tabs-link-selected not being removed from other tabs

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -21,7 +21,7 @@ class TabLink {
     const tabLinks = document.querySelectorAll('.tabs-link');
 
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-    Array.from(tabLinks).forEach(link => this.element.classList.remove('tabs-link-selected'));
+    Array.from(tabLinks).forEach(link => link.classList.remove('tabs-link-selected'));
 
     // Add a class named "tabs-link-selected" to this link
     this.element.classList.add('tabs-link-selected');
