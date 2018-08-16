45586d3c8c198766806e29927c16c399332d6eb1
decagondev <decagondevelopment@gmail.com>
2018-08-16 23:15:41 +0100

    finally it was a redicolous error on line 21 i had put an internal object instead of document as the object - fixed and working (charletta was a second pair of eyes and great moral support for this one - Thanks Charletta)

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -18,10 +18,11 @@ class TabLink {
 
   select() {
     // Get all of the elements with the tabs-link class
-    const links = this.element.querySelectorAll(".tabs-link");
-
+    const links = document.querySelectorAll(".tabs-link");
+    console.log(links);
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
     Array.from(links).forEach(link => {
+      console.log("array is running...");
       link.classList.remove("tabs-link-selected"); // it seems to be not working :/
     });
 
@@ -45,7 +46,6 @@ class TabItem {
     let items = document.querySelectorAll(".tabs-item");
     // Remove the class "tabs-item-selected" from each element
     items.forEach((item) => {
-      console.log("ITEM: " + item)
       item.classList.remove("tabs-item-selected");
     });
     // Add a class named "tabs-item-selected" to this element 
@@ -57,7 +57,6 @@ class TabItem {
 
 // START HERE: create a reference to the ".tabs" classes
 let links = document.querySelectorAll(".tabs-link"); // spent over an hour debugging cos of this typo
-
 // Following the code in the Dropdown file, iterate through the array you created above creating a new instance of the TabLink class for each item. 
 links = Array.from(links).map(link => { return new TabLink(link); });
 
