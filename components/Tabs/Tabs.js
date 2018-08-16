01b6d044e3cb362556e4c0dad5b27366346e1fd0
m-j-hacker <mjhacker@gmail.com>
2018-08-16 15:16:15 -0500

    Continuing work on Tabs.js - filled out Tablink class constructor

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -4,21 +4,21 @@ class TabLink {
     this.element = element;
 
     // Get the custom data attribute on the Link
-    this.data = this.element.dataset.
+    this.data = "this.element.dataset.tabs-link";
 
     // Using the custom data attribute get the associated Item element
-    this.itemElement;
+    this.itemElement = "this.element.dataset.tabs-item";
 
     // Using the Item element, create a new instance of the TabItem class
-    this.tabItem;
+    this.tabItem = new TabItem(this.tabItem);
 
     // Add a click event listener on this instance, calling the select method on click
-
+    this.element.addEventListener('click', () => { this.select()});
   };
 
   select() {
     // Get all of the elements with the tabs-link class
-    const links;
+    const links = this.querySelectorAll(".tabs-link");
 
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
     Array.from(links).forEach();
