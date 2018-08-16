3818e62b167c4ef3f2cc291910c850ca93412db0
Timothy Hoang <timh1203@gmail.com>
2018-08-16 18:14:04 -0500

    Add slide in animation

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -16,10 +16,27 @@ class Dropdown {
 
   // Toggle the ".dropdown-hidden" class off and on
   toggleContent() {
-    this.content.classList.toggle('dropdown-hidden')
+    this.content.classList.toggle('dropdown-hidden');
   }
 }
 
 // Nothing to do here, just study what the code is doing and move on to the Dropdown class
 let dropdowns = document.querySelectorAll('.dropdown');
 dropdowns = Array.from(dropdowns).map( dropdown => new Dropdown(dropdown));
+
+// Greensock
+const dropdownContent = document.querySelector('.dropdown-content');
+console.log(dropdownContent.classList.contains('dropdown-hidden'));
+document.querySelector('.dropdown-button').addEventListener('click', animateMenu);
+
+const slideOut = TweenMax.from(".dropdown-content", 2, {y:-100, opacity:0})
+function animateMenu() {
+  if (dropdownContent.classList.contains('dropdown-hidden')) {
+    // slideOut.reverse();
+    // console.log("hidden")
+    // TweenMax.from(".dropdown-content", 2, {y:-100, opacity:0}).reverse();
+  } else {
+    slideOut;
+    console.log("not hidden")
+  }
+}
