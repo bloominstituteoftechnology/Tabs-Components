8a596d92c23c97af69faac583b510ea993f7caa0
mparedes003 <michelle.g.paredes@gmail.com>
2018-08-16 16:07:55 -0700

    completed the rest of Tabs.js upto class TabItem. still working on select() under it.

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -13,33 +13,40 @@ class TabLink {
     
     // Using the Item element, create a new instance of the TabItem class
     // this.tabItem;
+    this.tabItem = new TabItem(this.itemElement);
     
     // Add a click event listener on this instance, calling the select method on click
-    // this.element.addEventListener('click', () => { this.linkClick() });
+    this.item.addEventListener('click', () => { this.select() });
   };
 
   select() {
     // Get all of the elements with the tabs-link class
-    // const links;
-
+    const links = document.querySelectorAll('.tabs-link');
+   
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-    // Array.from(links).forEach();
+    Array.from(links).forEach((e) => { e.classList.remove('tabs-link-selected');
+
+     });
 
     // Add a class named "tabs-link-selected" to this link
-    // this.element;
+    this.item.classList.add('tabs-link-selected');
     
     // Call the select method on the item associated with this link
+    this.tabItem.select();
 
   }
 }
 
 class TabItem {
   constructor(tabsItem) {
-    // this.element = tabsItem;
+    this.element = tabsItem;
   }
 
   select() {
     // Select all items elements from the DOM
+    let items = document.querySelectorAll('.tabs-item');
+    console.log(items);
+
     // Remove the class "tabs-item-selected" from each element
     // Add a class named "tabs-item-selected" to this element 
   }
