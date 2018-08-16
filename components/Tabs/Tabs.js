e4c66c6fa0887d3fe84b71b99d06f43ee6a307c0
Joseph Bradley <josephbradley@Josephs-MacBook-Air.local>
2018-08-16 13:34:16 -0700

    dropdown work

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -2,15 +2,15 @@
 class TabLink {
   constructor(element) {
     this.element = element;
-    
+    //Linked each element from mapped array.
     // Get the custom data attribute on the Link
     this.linkData = this.element.dataset.tab;
-    
+    //Created link to custom data
     // Using the custom data attribute get the associated Item element
-    this.itemElement = document.querySelector(`.tabs-link[data-tab]='${this.linkData}]`);
+    this.itemElement = document.querySelector(`.tabs-link[data-tab= ${this.linkData}]`);
     
     // Using the Item element, create a new instance of the TabItem class
-    this.tabItem;
+    this.itemElement = new TabItem(this.itemElement);
     
     // Add a click event listener on this instance, calling the select method on click
     this.element.addEventListener('click', () => {this.select()});
@@ -18,10 +18,10 @@ class TabLink {
 
   select() {
     // Get all of the elements with the tabs-link class
-    const links;
+    const links = document.querySelectorAll('.tabs-link');
 
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-    Array.from(links).forEach();
+    Array.from(links).forEach(link);
 
     // Add a class named "tabs-link-selected" to this link
     this.element;
@@ -46,8 +46,9 @@ class TabItem {
 
 // START HERE: create a reference to the ".tabs" classes
 let links = document.querySelectorAll('.tabs');
+//1 Created my nodeList psuedo array
 
 // Following the code in the Dropdown file, iterate through the array you created above creating a new instance of the TabLink class for each item. 
 links = Array.from(links).map(link => new TabLink(element));
-
+//2 Transformed into array and created new entry for class TabLink
 // DO THIS LAST: Once you have created an array of TabLink instances. call select() on the first item in the array
