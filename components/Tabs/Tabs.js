b3b6a91152ea1812db73a16573b9f5993a0e3866
m-j-hacker <mjhacker@gmail.com>
2018-08-16 16:15:48 -0500

    Continuing to work...

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -33,15 +33,16 @@ class TabLink {
 
 class TabItem {
   constructor(element) {
-    this.element;
+    this.element = element;
   }
 
   select() {
     // Select all items elements from the DOM
     let allItems = document.querySelectorAll(".tabs-item");
     // Remove the class "tabs-item-selected" from each element
-    this.element
+    Array.from(allItems).forEach(item => { item.classList.remove("tabs-item-selected") });
     // Add a class named "tabs-item-selected" to this element
+    this.element.classList.add("tabs-item-selected");
   }
 }
 
