0acfef3b50589967b88b96653226d98be6b52eba
Michael Agard <mdagard@gmail.com>
2018-08-16 15:30:29 -0400

    fixed reference error on line 43

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -21,10 +21,10 @@ class TabLink {
     const tabLinks = document.querySelectorAll('.tabs-link');
 
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-    Array.from(tabLinks).forEach(link => this.classList.remove('tabs-link-selected'));
+    Array.from(tabLinks).forEach(link => this.element.classList.remove('tabs-link-selected'));
 
     // Add a class named "tabs-link-selected" to this link
-    this.element.add('tabs-link-selected');
+    this.element.classList.add('tabs-link-selected');
     
     // Call the select method on the item associated with this link
     this.tabItem.select();
@@ -40,9 +40,9 @@ class TabItem {
     // Select all items elements from the DOM
     const items = document.querySelectorAll('.tabs-item');
     // Remove the class "tabs-item-selected" from each element
-    Array.from(items).forEach(item => this.classList.remove('tabs-item-selected'));
+    Array.from(items).forEach(item => item.classList.remove('tabs-item-selected'));
     // Add a class named "tabs-item-selected" to this element
-    this.element.add('tabs-item-selected');
+    this.element.classList.add('tabs-item-selected');
   }
 }
 
