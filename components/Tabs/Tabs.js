750ef23db412433f07362fad9cca48ecfd85a383
Joseph Bradley <josephbradley@Josephs-MacBook-Air.local>
2018-08-16 12:52:14 -0700

    Working through nodeList transformation and ingestion of new class

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -1,19 +1,19 @@
 
 class TabLink {
   constructor(element) {
-    this.element;
+    this.element = element;
     
     // Get the custom data attribute on the Link
-    this.data;
+    this.linkData = this.element.dataset.tab;
     
     // Using the custom data attribute get the associated Item element
-    this.itemElement;
+    this.itemElement = document.querySelector(`.tabs-link[data-tab]='${this.linkData}]`);
     
     // Using the Item element, create a new instance of the TabItem class
     this.tabItem;
     
     // Add a click event listener on this instance, calling the select method on click
-
+    this.element.addEventListener('click', () => {this.select()});
   };
 
   select() {
@@ -45,9 +45,9 @@ class TabItem {
 
 
 // START HERE: create a reference to the ".tabs" classes
-let links = document.querySelectorAll();
+let links = document.querySelectorAll('.tabs');
 
 // Following the code in the Dropdown file, iterate through the array you created above creating a new instance of the TabLink class for each item. 
-links = Array.from(links)
+links = Array.from(links).map(link => new TabLink(element));
 
 // DO THIS LAST: Once you have created an array of TabLink instances. call select() on the first item in the array
