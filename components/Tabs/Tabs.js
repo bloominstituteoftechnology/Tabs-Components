9b71670b869c2f96c9d946b6d41b7289be0b54b1
Joseph Bradley <josephbradley@Josephs-MacBook-Air.local>
2018-08-16 14:55:24 -0700

    working on tabs select method

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -7,10 +7,10 @@ class TabLink {
     this.linkData = this.element.dataset.tab;
     //Created link to custom data
     // Using the custom data attribute get the associated Item element
-    this.itemElement = document.querySelector(`.tabs-link[data-tab= ${this.linkData}]`);
+    this.itemElement = document.querySelectorAll(`.tabs-item[data-tab= '${this.linkData}]`);
     
     // Using the Item element, create a new instance of the TabItem class
-    this.itemElement = new TabItem(this.itemElement);
+    this.tabItem = new TabItem(this.itemElement);
     
     // Add a click event listener on this instance, calling the select method on click
     this.element.addEventListener('click', () => {this.select()});
@@ -21,19 +21,19 @@ class TabLink {
     const links = document.querySelectorAll('.tabs-link');
 
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-    Array.from(links).forEach(link);
+    Array.from(links).forEach(link  => link.classList.remove('tabs-link-selected'));
 
     // Add a class named "tabs-link-selected" to this link
-    this.element;
+    this.element.classList.add('tabs-link-selected');
     
     // Call the select method on the item associated with this link
-
+    this.tabItem.select();
   }
 }
 
 class TabItem {
   constructor(element) {
-    this.element;
+    this.element = element;
   }
 
   select() {
@@ -49,6 +49,6 @@ let links = document.querySelectorAll('.tabs-link');
 //1 Created my nodeList psuedo array
 
 // Following the code in the Dropdown file, iterate through the array you created above creating a new instance of the TabLink class for each item. 
-links = Array.from(links).map(link => new TabLink(link));
+links = Array.from(links).map( link => new TabLink(link));
 //2 Transformed into array and created new entry for class TabLink
 // DO THIS LAST: Once you have created an array of TabLink instances. call select() on the first item in the array
