45fc831c1b442fb1af6c6c5978aa4d0c7dae75e7
paulapivat <paul.apivat@gmail.com>
2018-09-06 22:54:51 +0700

    completed Part2

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -62,9 +62,10 @@ class TabLink {
     this.tab = this.element.dataset.tab;
     // console.log(this.tab)
     this.tabsItem = document.querySelector(`.tabs-item[data-tab="${this.tab}"]`);
-    
+
     this.tabsItemClass = new TabItem(this.tabsItem);
     this.element.addEventListener('click', () => {this.select()});
+    
   }
 
   select(){
@@ -78,7 +79,16 @@ class TabItem {
   }
 
   selectToggle() {
-    this.element.classList.toggle('tabs-item-selected')
+   // Get all of the elements with the tabs-link class
+   let links = document.querySelectorAll('.tabs-item');
+    
+   // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
+   Array.from(links).map((element) => element.classList.remove('tabs-item-selected'));
+
+   // Add a class named "tabs-link-selected" to this link
+  //  this.element.classList.add('tabs-item-selected');
+
+    this.element.classList.toggle('tabs-item-selected');
   }
 }
 
