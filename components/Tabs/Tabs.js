7f1f21ef694c51783789a033c8288618a0742b4b
MAbbas <mabbashm110@gmail.com>
2018-09-05 22:27:00 -0500

    Second part working through still

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -1,30 +1,32 @@
 
 class TabLink {
   constructor(element) {
-    this.element;
+    this.element = element;
     
     // Get the custom data attribute on the Link
-    this.data;
-    
+    this.data = this.element.dataset;
+    console.log(this.data);
+
     // Using the custom data attribute get the associated Item element
-    this.itemElement;
+    this.itemElement = document.querySelector(`.tabs-item[data-tab ="${this.number}]`);
     
     // Using the Item element, create a new instance of the TabItem class
-    this.tabItem;
+    this.tabItem = new tabItem(this.itemElement);
     
     // Add a click event listener on this instance, calling the select method on click
+    this.element.addEventListner('click', () => this.select());
+    console.log(this)
 
   };
-
   select() {
     // Get all of the elements with the tabs-link class
-    const links;
+    const links = Array.from(document.querySelectorAll('.tabs-link'));
 
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-    Array.from(links).forEach();
+    Array.from(links).forEach(item => item.classList.remove('tabs-link-selected'));
 
     // Add a class named "tabs-link-selected" to this link
-    this.element;
+    this.element.classList.add('tabs-link-selected');
     
     // Call the select method on the item associated with this link
 
@@ -33,7 +35,7 @@ class TabLink {
 
 class TabItem {
   constructor(element) {
-    this.element;
+    this.element = element;
   }
 
   select() {
@@ -45,7 +47,7 @@ class TabItem {
 
 
 // START HERE: create a reference to the ".tabs" classes
-let links = document.querySelectorAll();
+let links = document.querySelectorAll('.tabs');
 
 // Following the code in the Dropdown file, iterate through the array you created above creating a new instance of the TabLink class for each item. 
 links = Array.from(links)
