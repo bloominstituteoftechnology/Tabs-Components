1730ec8dbd47279a6ac8f028478a9b0f3a29c823
iepoch <staclem@live.com>
2018-09-05 22:56:12 -0700

    Fixed tabs am done!

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -22,13 +22,13 @@ class TabLink {
   select() {
    
     // // Get all of the elements with the tabs-link class
-    // const links = document.querySelectorAll('.tabs-link');
+    const links = document.querySelectorAll('.tabs-link');
     
-    // // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-    // Array.from(links).map((tabs) => tabs.classList.remove('tabs-link-selected'));
+    // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
+    Array.from(links).forEach((link) => {link.classList.remove('tabs-link-selected')});
 
-    // // Add a class named "tabs-link-selected" to this link
-    // this.tabItem.classList.add('tabs-link-selected');
+    // Add a class named "tabs-link-selected" to this link
+    this.element.classList.toggle('tabs-link-selected');
 
    
     // Call the select method on the item associated with this link
@@ -45,8 +45,11 @@ class TabItem {
 
   select() { //Switch
     // Select all items elements from the DOM
-   let item = document.querySelectorAll('.tabs-items');
-   this.item.classList.toggle('tabs-item-selected');
+  let item = document.querySelectorAll('.tabs-item');
+  Array.from(item).forEach((items) => items.classList.remove('tabs-item-selected'));
+
+  this.item.classList.toggle('tabs-item-selected');
+
     // Remove the class "tabs-item-selected" from each element
     // Add a class named "tabs-item-selected" to this element 
 
@@ -60,3 +63,4 @@ let links = document.querySelectorAll('.tabs-link');
 links = Array.from(links).map((link) => new TabLink(link));
 
 // DO THIS LAST: Once you have created an array of TabLink instances. call select() on the first item in the array
+links[0].select();
