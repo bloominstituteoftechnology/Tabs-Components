2b193787a4173085ca0f63919b6331f048c648f6
Lorenzo Evans <lorenzo.evans94@gmail.com>
2018-09-06 01:00:34 -0400

    Setting up elements for Tabs & functionality.

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -1,13 +1,13 @@
 
 class TabLink {
-  constructor(element) {
-    this.element;
+  constructor(links) {
+    this.element = links;
     
     // Get the custom data attribute on the Link
-    this.data;
+    this.data = this.element.dataset;
     
     // Using the custom data attribute get the associated Item element
-    this.itemElement;
+    this.itemElement = this.data.querySelectorAll();
     
     // Using the Item element, create a new instance of the TabItem class
     this.tabItem;
@@ -18,7 +18,7 @@ class TabLink {
 
   select() {
     // Get all of the elements with the tabs-link class
-    const links;
+    const links = document.querySelectorAll('tabs-link');
 
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
     Array.from(links).forEach();
@@ -45,9 +45,9 @@ class TabItem {
 
 
 // START HERE: create a reference to the ".tabs" classes
-let links = document.querySelectorAll();
+let links = document.querySelectorAll('.tabs');
 
 // Following the code in the Dropdown file, iterate through the array you created above creating a new instance of the TabLink class for each item. 
-links = Array.from(links)
+links = Array.from(links).map(links => new TabLink(links))
 
 // DO THIS LAST: Once you have created an array of TabLink instances. call select() on the first item in the array
