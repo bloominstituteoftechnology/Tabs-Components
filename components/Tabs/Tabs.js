fdd74e80b680ec3fc1766a57861d3b17d67e06be
Eric Whitcomb <eric.whitcomb@gmail.com>
2018-09-06 02:06:57 -0600

    Removed console.log lines

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -2,22 +2,21 @@
 class TabLink {
   constructor(element) {
     this.element = element;
-    console.log(this.element);
+    
     // Get the custom data attribute on the Link
     this.data = this.element.dataset.tab;
-    console.log(this.data);
+    
     // Using the custom data attribute get the associated Item element
     this.itemElement = document.querySelector(`.tabs-item[data-tab="${this.data}"]`);
-    console.log(this.itemElement);
+    
     // Using the Item element, create a new instance of the TabItem class
     this.tabItem = new TabItem(this.itemElement);
-    console.log(this.tabItem);
+   
     // Add a click event listener on this instance, calling the select method on click
     this.element.addEventListener('click', () => { this.select() });
   };
 
   select() {
-    console.log("check");
     // Get all of the elements with the tabs-link class
     const links = document.querySelectorAll('.tabs-link');;
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
