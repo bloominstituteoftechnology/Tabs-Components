5c32657e65a711b2ae1ffb7b3a757ceabed126ae
Emmanium <pradoe.ep@gmail.com>
2018-09-05 22:28:56 -0600

    finished Tabs.js, will go over all this again tomorrow

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -10,10 +10,10 @@ class TabLink {
     this.itemElement = document.querySelector(`.tabs-item[data-tab="${this.data}"]`);
     
     // Using the Item element, create a new instance of the TabItem class
-    this.tabItem;
+    this.tabItem = new TabItem(this.itemElement);
     
     // Add a click event listener on this instance, calling the select method on click
-
+    this.element.addEventListener('click', () => this.select());
   };
 
   select() {
@@ -24,31 +24,35 @@ class TabLink {
     Array.from(links).forEach(item => item.classList.remove('tabs-link-selected'));
 
     // Add a class named "tabs-link-selected" to this link
-    this.element;
+    this.element.classList.add('tabs-link-selected');
     
     // Call the select method on the item associated with this link
-
+    this.tabItem.select();
   }
 }
 
 class TabItem {
   constructor(element) {
-    this.element;
+    this.element = element;
   }
 
   select() {
     // Select all items elements from the DOM
+    const items = document.querySelectorAll('.tabs-item');
     // Remove the class "tabs-item-selected" from each element
-    // Add a class named "tabs-item-selected" to this element 
+    Array.from(items).forEach(item => item.classList.remove('tabs-item-selected'));
+    // Add a class named "tabs-item-selected" to this element
+    this.element.classList.add('tabs-item-selected'); 
   }
 }
 
 
-// START HERE: create a reference to the ".tabs" classes
-let links = document.querySelectorAll('.tabs');
+// START HERE: create a reference to the ".tabs-link" classes
+let links = document.querySelectorAll('.tabs-link');
 console.log(links);
 
 // Following the code in the Dropdown file, iterate through the array you created above creating a new instance of the TabLink class for each item. 
 links = Array.from(links).map(element => new TabLink(element));
 
 // DO THIS LAST: Once you have created an array of TabLink instances. call select() on the first item in the array
+links[0].select();
