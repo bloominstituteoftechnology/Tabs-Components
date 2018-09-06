8c262571546faceefe568981cbf9f8cff0c7c936
Julie Jonak <juliejonak10@gmail.com>
2018-09-05 23:25:53 -0500

    Problem solvin Tabs.js - issue with this.tabItem.addEventListener not being a function. Will continue tomorrow

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -23,13 +23,15 @@ class TabLink {
 
     // Using a loop or the forEach method remove the 'tabs-link-selected' class 
     //from all of the links
-    Array.from(links).forEach();
+    Array.from(links).forEach(function(){
+      this.element.classList.remove('tabs-link-selected');
+    });
 
     // Add a class named "tabs-link-selected" to this link
-    this.element;
+    this.element.classList.add('tabs-link-selected');
     
     // Call the select method on the item associated with this link
-
+    this.element.select();
   }
 }
 
@@ -40,8 +42,13 @@ class TabItem {
 
   select() {
     // Select all items elements from the DOM
+    const items = document.querySelectorAll('.tabs-items');
     // Remove the class "tabs-item-selected" from each element
+    Array.from(items).forEach(function(){
+      this.element.classList.remove('tabs-item-selected');
+    })
     // Add a class named "tabs-item-selected" to this element 
+    this.element.classList.add('tabs-item-selected');
   }
 }
 
@@ -51,7 +58,9 @@ let links = document.querySelectorAll('.tabs-link');
 
 // Following the code in the Dropdown file, iterate through the array you created 
 //above creating a new instance of the TabLink class for each item. 
-links = Array.from(links).map( tabs => new TabLink(tabs))
+links = Array.from(links).map( links => new TabLink(links))
 
 // DO THIS LAST: Once you have created an array of TabLink instances. call 
 //select() on the first item in the array
+
+links[0].select();
