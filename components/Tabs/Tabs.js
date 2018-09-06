30e877d89eb2b8e783ede3da578fd5d026b6415e
Courtney Buratto <you@example.com>
2018-09-05 23:02:54 -0600

    in the middle of part two, off to sleep

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -1,26 +1,27 @@
 
 class TabLink {
   constructor(element) {
-    this.element;
+    this.element = element;
     
     // Get the custom data attribute on the Link
-    this.data;
+    this.data = this.element.dataset.tab;
     
     // Using the custom data attribute get the associated Item element
-    this.itemElement;
+    this.itemElement= document.querySelector(`.tabs-item[data-tab = "${this.data}"]`);
     
     // Using the Item element, create a new instance of the TabItem class
-    this.tabItem;
+    this.tabItem = new TabItem(this.itemElement);
     
     // Add a click event listener on this instance, calling the select method on click
-
+    this.element.addEventListener("click", () => 
+      this.select()) 
   };
 
   select() {
     // Get all of the elements with the tabs-link class
-    const links;
+    const links= document.querySelectorAll(".tabs-link");
 
-    // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
+    //BEGIN HERE Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
     Array.from(links).forEach();
 
     // Add a class named "tabs-link-selected" to this link
