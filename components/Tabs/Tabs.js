778573cef502dda1037e6a2d82a6aa87bdbdae43
iepoch <staclem@live.com>
2018-09-05 22:00:14 -0700

    Fixed the Null issue.

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -5,14 +5,14 @@ class TabLink {
 
     // Get the custom data attribute on the Link
     this.data = this.element.dataset.tab;
-    console.log(this.data)
+ 
    
     // Using the custom data attribute get the associated Item element
-    this.itemElement = document.querySelector(`.tabs-item[data-tab="${this.data}]`);
-    console.log(this.itemElement);
+    this.itemElement = document.querySelector(`.tabs-item[data-tab="${this.data}"]`);
+    
     // Using the Item element, create a new instance of the TabItem class
     this.tabItem = new TabItem(this.itemElement);
-
+    console.log(this.tabItem);
     // Add a click event listener on this instance, calling the select method on click
      this.element.addEventListener('click', () => {
        this.select()
