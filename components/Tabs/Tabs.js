21c5048e57ec36db04ba9c5d21f3b7c22c63e68f
Lorenzo Evans <lorenzo.evans94@gmail.com>
2018-09-06 13:55:59 -0400

    Working on grabbing/using data attributes properly.

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -4,11 +4,11 @@ class TabLink {
     this.element = links;
     
     // Get the custom data attribute on the Link
-    this.data = this.element.dataset;
-    
+    this.data = this.element.dataset.tab;
+    console.log('this is the data: ', this.data);
     // Using the custom data attribute get the associated Item element
-    this.itemElement = this.data.querySelectorAll();
-    
+    this.itemElement = this.data.querySelectorAll(`.tabs-links[data-tab= "${this.data-tab}]`);
+    // console.log(this.itemElement);
     // Using the Item element, create a new instance of the TabItem class
     this.tabItem;
     
@@ -18,7 +18,7 @@ class TabLink {
 
   select() {
     // Get all of the elements with the tabs-link class
-    const links = document.querySelectorAll('tabs-link');
+    // const links = document.querySelectorAll('tabs-link');
 
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
     Array.from(links).forEach();
@@ -46,8 +46,8 @@ class TabItem {
 
 // START HERE: create a reference to the ".tabs" classes
 let links = document.querySelectorAll('.tabs');
-
+console.log(links)
 // Following the code in the Dropdown file, iterate through the array you created above creating a new instance of the TabLink class for each item. 
 links = Array.from(links).map(links => new TabLink(links))
-
+console.log(links);
 // DO THIS LAST: Once you have created an array of TabLink instances. call select() on the first item in the array
