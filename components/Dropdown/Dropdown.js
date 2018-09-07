ea10fa5f43b4d01e9c51deca0e64de9b240054d9
jbrock1981 <jbrock1981@gmail.com>
2018-09-06 22:07:09 -0500

    .

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -5,29 +5,29 @@ class Dropdown {
     this.element = element;
     
     // Get the element with the ".dropdown-button" class found in the dropdown element (look at the HTML for context)
-    this.button = this.element.querySelector(".dropdown-button");
+    this.button = this.element.querySelector('.dropdown-button');
     
     // assign the reference to the ".dropdown-content" class found in the dropdown element
-    this.content = this.element.querySelector(".dropdown-content");
+    this.dropDownContent = this.element.querySelector('.dropdown-content');
     
     // Add a click handler to the button reference and call the toggleContent method.
     this.button.addEventListener('click', () => {
-      this.toggleContent();
+      return this.toggleDropDownContent();
     })
   }
-
-  toggleContent() {
-    
+  toggleDropDownContent() {
     // Toggle the ".dropdown-hidden" class off and on
-    this.content.classList.toggle('dropdown-hidden');
-    /*
-    if (this.content.classList.contains("dropdown-hidden")) {
-      TweenMax.to(menu, .25, {top: -250, opacity: 0, onComplete: () => {this.content.classList.remove("dropdown-hidden")}});
-     } else {
-      TweenMax.to(menu, .25, {top: 0, opacity: 1, onComplete: this.content.classList.add("dropdown-hidden")});
-     }*/
+    let display = this.dropDownContent.style.display;
+    if (display === "flex") {
+    this.dropDownContent;
+      TweenMax.fromTo(this.dropDownContent, 1, {y: 0, opacity: 1, display: "flex"}, {y: -150, opacity: 0, display: "none"});
+    } else {
+      TweenMax.fromTo(this.dropDownContent, 1, {y: -150, opacity: 0, display: "none"}, {y: 0, opacity: 1, display: "flex"});
+    }
   }
 }
+
+
 
 // Nothing to do here, just study what the code is doing and move on to the Dropdown class
 let dropdowns = document.querySelectorAll('.dropdown');
