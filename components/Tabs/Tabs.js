7891d7a35be0d79f39a08eb1545ed1b60ce41556
rlmclaughlin <rlmclaughlin@yahoo.com>
2018-09-06 22:40:04 -0500

    made changes to tabs.js

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -21,7 +21,7 @@ class TabLink {
     
     const links = document.querySelectorAll('.tabs-link');
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-    Array.from(links).forEach(link => link.classList.remove('tabs-link-selected'));
+    Array.from(links).forEach(link => {link.classList.remove('tabs-link-selected')});
 
     // Add a class named "tabs-link-selected" to this link
     this.element.classList.add('tabs-link-selected');
@@ -54,7 +54,7 @@ class TabItem {
 let links = document.querySelectorAll(".tabs-link");
 
 // Following the code in the Dropdown file, iterate through the array you created above creating a new instance of the TabLink class for each item. 
-links = Array.from(links);
+links = Array.from(links).map(link => new TabLink(link));
 
 links[0].select();
 // DO THIS LAST: Once you have created an array of TabLink instances. call select() on the first item in the array
