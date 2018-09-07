c6569f125c3f0313ae235e07bb790cb00204c46c
Colin Dismuke <colin.dismuke@gmail.com>
2018-09-06 22:54:15 -0500

    Changed name of forEach element in TabLink.js to el from link.

--- components/Tabs/TabLink.js
+++ components/Tabs/TabLink.js
@@ -23,8 +23,8 @@ class TabLink {
     console.log(links)
 
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-    Array.from(links).forEach( link => {
-      link.classList.remove('tabs-link-selected');
+    Array.from(links).forEach( el => {
+      el.classList.remove('tabs-link-selected');
     });
 
     // Add a class named "tabs-link-selected" to this link
