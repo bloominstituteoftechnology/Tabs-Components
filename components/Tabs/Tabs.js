b8f5da13e9f813c612397c3c0017ae6fa5c2dac3
Robert Billington <chillingtonbillington@gmail.com>
2018-09-06 21:27:01 -0500

    mostly complete tabs.js

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -7,7 +7,7 @@ class TabLink {
     this.data = this.element.dataset.tab;
     
     // Using the custom data attribute get the associated Item element
-    this.itemElement = document.querySelector(`tabs-item[data-tab = "${this.data}"]`);
+    this.itemElement = document.querySelector(`.tabs-item[data-tab = "${this.data}"]`);
     
     // Using the Item element, create a new instance of the TabItem class
     this.tabItem = new TabItem(this.itemElement);
@@ -18,34 +18,41 @@ class TabLink {
 
   select() {
     // Get all of the elements with the tabs-link class
-    const links;
+    const links = document.querySelector('.tabs-links');
 
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-    Array.from(links).forEach();
+    Array.from(links).forEach(link =>{
+      link.element.classList.remove('tabs-link-selected');
+    });
 
     // Add a class named "tabs-link-selected" to this link
-    this.element;
+    this.element.classList.toggle('tabs-link-selected');
     
     // Call the select method on the item associated with this link
-
+    this.tabItem.select();
   }
 }
 
 class TabItem {
   constructor(element) {
-    this.element;
+    this.element = element;
   }
 
   select() {
     // Select all items elements from the DOM
+    let items = document.querySelectorAll('.tabs-item')
     // Remove the class "tabs-item-selected" from each element
+    Array.from(items).forEach(item =>{
+      item.classList.remove('tabs-item-selected');
+    })
     // Add a class named "tabs-item-selected" to this element 
+    this.element.classList.toggle('tabs-item-selected')
   }
 }
 
 
 // START HERE: create a reference to the ".tabs" classes
-let links = document.querySelectorAll('.tabs');
+let links = document.querySelectorAll('.tabs-link');
 
 // Following the code in the Dropdown file, iterate through the array you created above creating a new instance of the TabLink class for each item. 
 links = Array.from(links).map(link => new TabLink(link));
