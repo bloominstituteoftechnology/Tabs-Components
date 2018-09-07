df3fd4dc20649703fcb83639f324fd870fa33f85
Jesse <theiseja@gmail.com>
2018-09-06 21:07:12 -0500

    Completed MVP, Working on some stretch stuff now

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -2,7 +2,7 @@ class Dropdown {
   constructor(element) {
     
     // Assign this.element to the dropdown element
-    this.element;
+    this.element = element;
     
     // Get the element with the ".dropdown-button" class found in the dropdown element (look at the HTML for context)
     this.button = this.element.querySelector('.dropdown-button');
@@ -11,15 +11,12 @@ class Dropdown {
     this.content = this.element.querySelector('.dropdown-content');
     
     // Add a click handler to the button reference and call the toggleContent method.
-    this.button.addEventListener('click', () => {
-      
-    })
+    this.button.addEventListener('click', () => { this.toggleContent(); })
   }
-
   toggleContent() {
     
     // Toggle the ".dropdown-hidden" class off and on
-    this.content;
+    this.content.classList.toggle('dropdown-hidden');
   }
 }
 
