14a2386b7474a1edc2882c46b77ff6d1cd8a4b14
Jess <jessdev83@gmail.com>
2018-09-06 22:21:56 -0400

    Changed Tabs.js
    Changed forEach() methods in both select() methods to map().

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -20,7 +20,7 @@ class TabLink {
         const links = document.querySelectorAll('.tabs-link');
 
         // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-        Array.from(links).forEach((link) => {
+        Array.from(links).map((link) => {
             link.classList.remove('tabs-link-selected');
         });
 
@@ -42,7 +42,7 @@ class TabItem {
         // Remove the class "tabs-item-selected" from each element
         // Add a class named "tabs-item-selected" to this element 
         const items = document.querySelectorAll('.tabs-item');
-        Array.from(items).forEach((tab) => {
+        Array.from(items).map((tab) => {
             tab.classList.remove('tabs-item-selected');
         });
         this.element.classList.add('tabs-item-selected');
