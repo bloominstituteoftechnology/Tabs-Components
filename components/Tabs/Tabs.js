c0b7d4c96e5a452592336f654315e21f92a1672c
jbierly1 <james.bierly@gmail.com>
2018-09-06 22:41:07 -0400

    finished part one

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -18,7 +18,7 @@ class TabLink {
 
   select() {
     // Get all of the elements with the tabs-link class
-    const links;
+    // const links;
 
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
     Array.from(links).forEach();
