d5e816e0bb31bbbad902a9b72a6dac58a25a33c0
TheGameFreak720 <luis.alvarez@commithub.com>
2018-09-06 21:48:29 -0400

    fixed typo on line 43

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -40,7 +40,7 @@ class TabItem {
     // Select all items elements from the DOM
     const items = document.querySelectorAll('.tabs-items');
     // Remove the class "tabs-item-selected" from each element
-    Array.from(items).forEach(item => item.classList.remove('tabs-link-selected'));
+    Array.from(items).forEach(item => item.classList.remove('tabs-item-selected'));
     // Add a class named "tabs-item-selected" to this element 
     this.element.classList.add('tabs-item-selected');
   }
