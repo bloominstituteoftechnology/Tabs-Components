148ee3215c767884053d52b73dac221583485c1c
Wesley Meeks <wesmeeks88@gmail.com>
2018-09-06 20:16:18 -0600

    completed dropdown.js and tabs.js, added two tabs for practice.

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -20,7 +20,7 @@ class Dropdown {
   toggleContent() {
 
     // Toggle the ".dropdown-hidden" class off and on
-    this.content.classList.toggle('.dropdown-hidden');
+    this.content.classList.toggle('dropdown-hidden');
   }
 }
 
