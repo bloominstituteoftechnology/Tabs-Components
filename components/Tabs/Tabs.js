6c1362fd0d4336f1f739e4d719f7fccd4155e2e6
hraya <hraya@lssu.edu>
2018-09-11 00:05:49 -0500

    Added missing Tabs
    did not notice I had to add tabs
    
    They are now added accordingly

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -7,7 +7,7 @@ class TabLink {
 
     // Using the custom data attribute get the associated Item element
     this.itemElement = document.querySelector(`.tabs-item[data-tab='${this.data}'`);
-
+    console.log(this.itemElement)
     // Using the Item element, create a new instance of the TabItem class
     this.tabItem = new TabItem(this.itemElement);
 
