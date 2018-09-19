fdec5100e03341400e535026a27e269f4e8bd102
Brandon Vilaychith <blvilaychith@outlook.com>
2018-09-19 15:08:15 -0500

    toggleContent added to this.content

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -19,7 +19,7 @@ class Dropdown {
   toggleContent() {
     
     // Toggle the ".dropdown-hidden" class off and on
-    this.content;
+    this.content.classList.toggle('dropdown-hidden');
   }
 }
 
