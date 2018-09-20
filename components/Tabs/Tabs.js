bb89a6277138e5024e1a365d83ecac12f543b3ab
Lee Len Formento <leebatef@gmail.com>
2018-09-20 16:02:09 -0400

    Articles. also shortened array.from

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -52,9 +52,8 @@ class TabItem {
 let links = document.querySelectorAll('.tabs-link');
 
 // Following the code in the Dropdown file, iterate through the array you created above creating a new instance of the TabLink class for each item. 
-links = Array.from(links).map(link => {
-  return new TabLink(link);
-});
+links = Array.from(links).map(links => new TabLink(links))
+
 
 // DO THIS LAST: Once you have created an array of TabLink instances. call select() on the first item in the array
 links[0].select();
