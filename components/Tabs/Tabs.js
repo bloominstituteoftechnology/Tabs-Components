6f6c3c13aa608b2115e8569592a4a1cc4be0a726
Michael Checo <checomichael2@gmail.com>
2018-09-20 19:00:00 -0400

    fixed my select methods

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -6,8 +6,7 @@ class TabLink {
     this.data = this.element.dataset.tab;
 
     // Using the custom data attribute get the associated Item element
-    this.itemElement = document.querySelector(
-      `.tabs-item[data-tab="${this.data}"]`
+    this.itemElement = document.querySelector(`.tabs-item[data-tab="${this.data}"]`
     );
 
     // Using the Item element, create a new instance of the TabItem class
@@ -17,19 +16,13 @@ class TabLink {
     this.element.addEventListener("click", () => {
       this.select();
     });
-  }
+  };
 
   select() {
     // Get all of the elements with the tabs-link class
-    let links = document.querySelectorAll('.tabs-link');
-
-    // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-    links = Array.from(links).forEach(link => link.classList.remove('tabs-link-selected'));
-
-    // Add a class named "tabs-link-selected" to this link
-    // this.element.classList.toggle("tabs-link-selected");
-
-    // Call the select method on the item associated with this link
+    const links = document.querySelectorAll('.tabs-link');
+   links.forEach(link => link.classList.remove('tabs-link-selected'));
+    this.element.classList.add('tabs-link-selected');
     this.itemElement.select();
   }
 }
@@ -40,12 +33,9 @@ class TabItem {
   }
 
   select() {
-    // Select all items elements from the DOM
-    this.itemElement.classList.toggle("tabs-item-selected");
-
-    // Remove the class "tabs-item-selected" from each element
-
-    // Add a class named "tabs-item-selected" to this element
+    const content = document.querySelectorAll('.tabs-item');
+    Array.from(content).forEach((item) => item.classList.remove('tabs-item-selected'));
+    this.itemElement.classList.add('tabs-item-selected');
   }
 }
 
