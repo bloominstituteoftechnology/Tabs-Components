b70eaa4af239be94fcd1de611287cd841df24852
Brandon Strellis <bstrellis@gmail.com>
2018-09-20 18:18:23 -0400

    quotes appearing but classes not updating properly

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -5,11 +5,10 @@ class TabLink {
     
     // Get the custom data attribute on the Link
     this.data = this.element.dataset.tab;
-    console.log('this.data', this.data);
     
     // Using the custom data attribute get the associated Item element
     this.itemElement = document.querySelector(`.tabs-item[data-tab='${this.data}']`);
-    console.log('this.itemElement', this.itemElement);
+
     // Using the Item element, create a new instance of the TabItem class
     this.tabItem = new TabItem(this.itemElement);
     
@@ -19,30 +18,32 @@ class TabLink {
 
   select() {
     // Get all of the elements with the tabs-link class
-    const links = this.element.querySelectorAll('.tab-link');
-    console.log('in select for TabLink');
+    const links = document.querySelectorAll('.tabs-link');
 
     // Using a loop or the forEach method remove the 'tabs-link-selected' 
     // class from all of the links
-    Array.from(links).forEach((element) => this.link.classList.remove('tabs-link-selected'));
+    Array.from(links).forEach((link) => link.classList.remove('tabs-link-selected'));
 
     // Add a class named "tabs-link-selected" to this link
     this.element.classList.add('tabs-link-selected');
     
     // Call the select method on the item associated with this link
-
+    this.tabItem.select();
   }
 }
 
 class TabItem {
   constructor(element) {
-    this.element;
+    this.element = element;
   }
 
   select() {
     // Select all items elements from the DOM
+    const allItems = document.querySelectorAll('.tabs-item');
     // Remove the class "tabs-item-selected" from each element
+    Array.from(allItems).map( item => item.classList.remove('.tabs-item-selected'));
     // Add a class named "tabs-item-selected" to this element 
+    this.element.classList.add('tabs-item-selected');
   }
 }
 
