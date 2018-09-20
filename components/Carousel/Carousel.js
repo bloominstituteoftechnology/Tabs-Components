7638f36a3138a4d6e35eb6bd1d3d8c4ca5a9c3b9
Peter Pham <phamhere@gmail.com>
2018-09-20 15:58:14 -0600

    added GSAP and tabitem fadein

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -28,6 +28,7 @@ class Carousel {
       this.current = 0;
     }
     this.items[this.current].classList.add('box-active');
+    TweenMax.from(this.items[this.current], 2, {opacity: 0});
     this.buttonSpan.innerText = this.current + 1;
   }
 }
