1bfa0da8c2b15b023fb3d10a6225a55c6787f6be
bummings <sup@fauxkno.com>
2018-09-20 17:50:20 -0400

    almost thru with tabs

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -44,9 +44,11 @@ class TabItem {
 
 
 // START HERE: create a reference to the ".tabs" classes
-let links = document.querySelectorAll();
+let links = document.querySelectorAll('.tabs');
 
 // Following the code in the Dropdown file, iterate through the array you created above creating a new instance of the TabLink class for each item. 
-links = Array.from(links)
+links = Array.from(links).map(link => {
+  return new TabLink(link);
+})
 
 // DO THIS LAST: Once you have created an array of TabLink instances. call select() on the first item in the array
