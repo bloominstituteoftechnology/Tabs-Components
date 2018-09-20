b99228e4d77a9debc42bd94147284648657190f0
Hunter315 <hunter.mckay.smith@gmail.com>
2018-09-20 14:59:15 -0600

    Fixed typo in dropwdown.js

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -41,17 +41,26 @@ class TabItem {
   }
 
   select() {
+    
     // Select all items elements from the DOM
+    const items = document.querySelectorAll('.tabs-item');
     // Remove the class "tabs-item-selected" from each element
+    Array.from(items).forEach(function(element){
+      element.classList.remove('tabs-item-selected');
+    });
     // Add a class named "tabs-item-selected" to this element 
+    this.element.classList.add('tabs-item-selected');
   }
 }
 
 
 // START HERE: create a reference to the ".tabs" classes
-let links = document.querySelectorAll();
+let links = document.querySelectorAll('.tabs-link');
 
 // Following the code in the Dropdown file, iterate through the array you created above creating a new instance of the TabLink class for each item. 
-links = Array.from(links)
+links = Array.from(links).map(element => {
+  return new TabLink(element);
+});
 
 // DO THIS LAST: Once you have created an array of TabLink instances. call select() on the first item in the array
+links[0].select();
