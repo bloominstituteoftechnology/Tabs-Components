27440be001e24af86834f6bfcaee66729e16983c
Corey Gumbs <cgumbs@Coreys-MBP-3.fios-router.home>
2018-09-20 18:44:54 -0400

    [fix] fixed links error/called wrong class selector

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -2,15 +2,16 @@
 class TabLink {
   constructor(element) {
     this.element = element;
-   
+    
     // Get the custom data attribute on the Link
-    this.data = this.element.querySelector(".tabs-link").dataset.tab;
+    this.data = this.element.dataset.tab;
+    console.log(this.data)
     
     // Using the custom data attribute get the associated Item element
     this.itemElement;
     
     // Using the Item element, create a new instance of the TabItem class
-    this.tabItem;
+    this.tabItem = new TabItem(this.data);
     
     // Add a click event listener on this instance, calling the select method on click
 
@@ -45,9 +46,10 @@ class TabItem {
 
 
 // START HERE: create a reference to the ".tabs" classes
-let links = document.querySelectorAll(".tabs");
+let links = document.querySelectorAll(".tabs-link");
+
 
 // Following the code in the Dropdown file, iterate through the array you created above creating a new instance of the TabLink class for each item. 
-links = Array.from(links).map(link => new TabLink(link));
+links = Array.from(links).map(link =>  new TabLink(link));
 
 // DO THIS LAST: Once you have created an array of TabLink instances. call select() on the first item in the array
