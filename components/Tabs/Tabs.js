351ec45638d0cba7c9bc8cf28cc564960c576316
Steven O'Neill <oneillsteven957@gmail.com>
2018-09-20 17:23:42 -0500

    Extra tabs added

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -13,8 +13,8 @@ class TabLink {
     this.tabItem = new TabItem(this.itemElement);
     
     // Add a click event listener on this instance, calling the select method on click
-    this.element.addEventListener('click', () => {
-      this.select()
+    this.element.addEventListener('click', (event) => {
+      this.select(event)
     });
   };
 
