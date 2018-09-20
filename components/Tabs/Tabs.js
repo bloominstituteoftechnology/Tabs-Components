64e6730344fa9a4dea55bd7e9fdb9431e7071b14
Mindysueasaurus <mymindyyeager@gmail.com>
2018-09-20 17:16:21 -0500

    bugs with tabs resolved

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -13,7 +13,7 @@ class TabLink {
     this.tabItem = new TabItem(this.itemElement);
    
     // Add a click event listener on this instance, calling the select method on click
-    this.element.addEventListener('click', () => this.select(event));
+    this.element.addEventListener('click', () => this.select());
     
   };
 
@@ -23,14 +23,14 @@ class TabLink {
     
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
      Array.from(links).forEach( (link) => {
-     this.element.classList.remove('tabs-link-selected')
+        link.classList.remove('tabs-link-selected')
     });
 
     // Add a class named "tabs-link-selected" to this link
-    this.element.classList.toggle('tabs-link-selected');
+        this.element.classList.add('tabs-link-selected');
     
     // Call the select method on the item associated with this link
-      this.tabItem.select();
+        this.tabItem.select();
   }
 }
 
@@ -41,10 +41,10 @@ class TabItem {
 
   select() {
     // Select all items elements from the DOM
-    document.querySelectorAll('.tabs-item')
+    let elements = document.querySelectorAll('.tabs-item')
     // Remove the class "tabs-item-selected" from each element
-    Array.from(links).forEach( (links) => {
-      this.element.classList.remove("tabs-item-selected");
+    Array.from(elements).forEach( (element) => {
+     element.classList.remove("tabs-item-selected");
     })
     
     // Add a class named "tabs-item-selected" to this element 
