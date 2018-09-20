430252fbc6f2509e7944d625e3495447618259d0
SeanOne <basement365@gmail.com>
2018-09-20 15:59:20 -0700

    still working on tab js but i need to turn in stand up

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -2,13 +2,12 @@
 class TabLink {
   constructor(element) {
     this.element = element;
-
     // Get the custom data attribute on the Link
     this.data = element.dataset.tab;
-
+    
     // Using the custom data attribute get the associated Item element
     this.itemElement = document.querySelector(`.tabs-item[data-set='${this.data}']`);
-
+    console.log(this.itemElement);
     // Using the Item element, create a new instance of the TabItem class
     this.tabItem = new TabItem(this.itemElement);
 
@@ -19,30 +18,36 @@ class TabLink {
   select(event) {
     // Get all of the elements with the tabs-link class
     let links = event.target;
-    // console.log(event.target);
 
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
     Array.from(links).forEach((link) => {
-      console.log(link);
+      link.classList.remove('tabs-link-selected');
     });
 
     // Add a class named "tabs-link-selected" to this link
-    this.element;
+    this.element.classList.add('tabs-link-selected');
 
     // Call the select method on the item associated with this link
+    this.tabItem.select();
 
   }
 }
 
 class TabItem {
   constructor(element) {
-    this.element;
+    this.element = element;
   }
 
   select() {
     // Select all items elements from the DOM
     // Remove the class "tabs-item-selected" from each element
     // Add a class named "tabs-item-selected" to this element 
+    let tabsItem = document.querySelectorAll(".tabs-item");
+    tabsItem.forEach(tab => {
+      tab.classList.remove('tabs-item-selected');
+    });
+
+    element.classList.add('tabs-item-selected');
   }
 }
 
