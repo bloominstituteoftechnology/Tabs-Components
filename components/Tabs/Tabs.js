836a17e4e98fae2faf3c9e4e65467cb0a5e1e5e3
ThomasUtsey <tomutsey@yahoo.com>
2018-09-20 19:03:17 -0400

    work in progress

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -1,35 +1,38 @@
 
 class TabLink {
   constructor(element) {
-    this.element;
-    
+    this.element = element;
+     
     // Get the custom data attribute on the Link
-    this.data;
+    this.data = this.element.dataset.tab;
+    ;
     
     // Using the custom data attribute get the associated Item element
-    this.itemElement;
+    this.itemElement = document.querySelector(`.tabs-item[data-tab='${this.data}']`);
     
     // Using the Item element, create a new instance of the TabItem class
-    this.tabItem;
+    this.tabItem = new TabItem(this.itemElement);
     
     // Add a click event listener on this instance, calling the select method on click
-
+    this.element.addEventListener('click', () => {this.linkClick(event) });
   };
-
+  
   select() {
     // Get all of the elements with the tabs-link class
-    const links;
-
+    let links = document.querySelectorAll('.tabs-link');
+    
+    
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-    Array.from(links).forEach();
+    Array.from(links).forEach((items) => items.classList.remove('tabs-link-selected'));
+  
 
     // Add a class named "tabs-link-selected" to this link
     this.element;
     
     // Call the select method on the item associated with this link
-
   }
-}
+  }
+
 
 class TabItem {
   constructor(element) {
@@ -45,9 +48,12 @@ class TabItem {
 
 
 // START HERE: create a reference to the ".tabs" classes
-let links = document.querySelectorAll();
+let links = document.querySelectorAll(".tabs-link");
 
 // Following the code in the Dropdown file, iterate through the array you created above creating a new instance of the TabLink class for each item. 
-links = Array.from(links)
+links = Array.from(links).map(tabs => new TabLink(tabs));
+console.log(links);
+//dropdowns = Array.from(dropdowns).map( dropdown => new Dropdown(dropdown));
+// console.log(dropdowns);
 
 // DO THIS LAST: Once you have created an array of TabLink instances. call select() on the first item in the array
