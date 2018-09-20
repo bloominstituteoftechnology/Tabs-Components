8c593e30361e9e1326ebbb6824d966472dbb6c88
SeanOne <basement365@gmail.com>
2018-09-20 14:46:57 -0700

    code got away from me | reset tabs.js, starting again

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -2,16 +2,16 @@
 class TabLink {
   constructor(element) {
     this.element;
-    
+
     // Get the custom data attribute on the Link
     this.data;
-    
+
     // Using the custom data attribute get the associated Item element
     this.itemElement;
-    
+
     // Using the Item element, create a new instance of the TabItem class
     this.tabItem;
-    
+
     // Add a click event listener on this instance, calling the select method on click
 
   };
@@ -25,7 +25,7 @@ class TabLink {
 
     // Add a class named "tabs-link-selected" to this link
     this.element;
-    
+
     // Call the select method on the item associated with this link
 
   }
