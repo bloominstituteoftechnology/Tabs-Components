8c666c851380d0122efe6cc1ef9245b6387ae400
Drewsup123c <drewsup123c@gmail.com>
2018-09-20 13:37:20 -0600

    added notes to Dropdown.js for my understanding

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -1,6 +1,5 @@
 class Dropdown {
   constructor(element) {
-    
     // Assign this.element to the dropdown element
     this.element = document.querySelector('.dropdown');
     // Get the element with the ".dropdown-button" class found in the dropdown element (look at the HTML for context)
@@ -23,3 +22,11 @@ class Dropdown {
 // Nothing to do here, just study what the code is doing and move on to the Dropdown class
 let dropdowns = document.querySelectorAll('.dropdown');
 dropdowns = Array.from(dropdowns).map( dropdown => new Dropdown(dropdown));
+
+// My notes on this
+
+//line 23
+  // Setting a variable called dropdowns to the html element(s) with the class dropdown: It is now a node lise
+//line 24
+  // the dropdown node list is being turned into an array using array.from(dropdowns)
+  // We are mapping the now array to be a new object with the name Dropdown and passing in our original variable
