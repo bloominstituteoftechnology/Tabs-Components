e95026dc6bf360ee4f3bf1bc493d4deb329800ac
Phil Hart <phil@Phils-iMac.local>
2018-09-20 17:00:44 -0600

    Still on the tabs.js

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -8,18 +8,16 @@ class Dropdown {
     this.button = this.element.querySelector('.dropdown-button');
     
     // assign the reference to the ".dropdown-content" class found in the dropdown element
-    this.content = element.querySelector('dropdown-content');
+    this.content = document.querySelector('.dropdown-content');
     
     // Add a click handler to the button reference and call the toggleContent method.
-    this.button.addEventListener('click', () => {this.toggleContent()
-
-    })
+    this.button.addEventListener('click', () => {this.toggleContent()})
   }
 
   toggleContent() {
     
     // Toggle the ".dropdown-hidden" class off and on
-    this.content.classlist.toggle('dropdown-hidden');
+    this.content.classList.toggle('dropdown-hidden');
   }
 }
 
