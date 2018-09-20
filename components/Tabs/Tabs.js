2036b5c1478fb5aa9fb303955e8f0dccc56b6ee4
pedro <pedroluismontesinosrivera@gmail.com>
2018-09-20 16:42:44 -0400

    iretated through all the elements with the tab-link class and removed the class tabs-link-selected, then I was asked to add a class named tabs-link-selected to the same link

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -19,16 +19,18 @@ class TabLink {
 
   select() {
     // Get all of the elements with the tabs-link class
-    const links;
+    const links = document.querySelectorAll(".tabs-link");
 
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-    Array.from(links).forEach();
+    Array.from(links).forEach((link) => {
+      link.classList.remove("tabs-link-selected");
+    });
 
     // Add a class named "tabs-link-selected" to this link
-    this.element;
+    this.element.classList.add("tabs-link-selected");
     
     // Call the select method on the item associated with this link
-
+    this.element.select();
   }
 }
 
