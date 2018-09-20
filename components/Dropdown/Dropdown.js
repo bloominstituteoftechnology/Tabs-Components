9d16490d0fb8e1838d2c1d4600c2436bc4400e06
Adam Hinckley <adamhinckley@Adams-MacBook-Pro.local>
2018-09-20 12:38:53 -0600

    First Commit

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -4,7 +4,6 @@ class Dropdown {
     // Assign this.element to the dropdown element
     this.element = document.querySelector('.dropdown');
 
-    // Get the element with the ".dropdown-button" class found in the dropdown element (look at the HTML for context)
     this.button = this.element.querySelector('.dropdown-button');
 
 
