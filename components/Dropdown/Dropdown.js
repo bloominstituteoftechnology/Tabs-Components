3fa3471ebfd8f44eaa92a89d33346e84689e5559
theryanclausen <theryanclausen@gmail.com>
2018-09-21 07:55:38 -0700

    carosel

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -24,3 +24,56 @@ class Dropdown {
 // Nothing to do here, just study what the code is doing and move on to the Dropdown class
 let dropdowns = document.querySelectorAll('.dropdown');
 dropdowns = Array.from(dropdowns).map( dropdown => new Dropdown(dropdown));
+
+class ImageItem {
+  constructor(element){
+    this.element = element;
+    this.leftBumper = document.createElement('div');
+    this.rightBumper = document.createElement('div');
+
+    this.leftBumper.innerHTML = `<h4>◁</h4>`;
+    this.leftBumper.classList.toggle('bumper-arrow');
+
+    this.rightBumper.innerHTML = `<h4>▷</h4>`;
+    this.rightBumper.classList.toggle('bumper-arrow');
+
+    this.element.prepend(this.leftBumper);
+    this.element.appendChild(this.rightBumper);
+    
+    this.data = this.element.dataset.pic;
+
+    // this.itemElement = document.querySelector(
+    //   `.tabs-item[data-tab='${this.data}']`
+    // );
+    this.leftBumper.addEventListener('click', () => this.goLastPic())
+    this.rightBumper.addEventListener('click', () => this.goNextPic())
+
+  }
+  goLastPic(){
+    this.element.classList.toggle('images-current');
+    if (this.data - 1 === 0){
+      this.lastPicData = 4 
+    }
+    else{
+      this.lastPicData = this.data - 1
+    }
+    this.lastPic = document.querySelector( `[data-pic= '${this.lastPicData}']`)
+    this.lastPic.classList.toggle('images-current')
+  }
+  goNextPic(){
+    this.element.classList.toggle('images-current');
+    if (Number(this.data) + 1 === 5){
+      this.nextPicData = 1;
+    }
+    else{
+      this.nextPicData = Number(this.data) + 1;
+    }
+    console.log(this.nextPicData)
+    this.nextPic = document.querySelector( `[data-pic= '${this.nextPicData}']`);
+    this.nextPic.classList.toggle('images-current');
+  }
+}
+
+let images = document.querySelectorAll('.images')
+
+images = Array.from(images).map(x => new ImageItem(x))
