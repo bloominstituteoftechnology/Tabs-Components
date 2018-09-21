78cec5b6deb0f02964259800b23b558cffc29291
bummings <sup@fauxkno.com>
2018-09-21 16:14:03 -0400

    completed functionality, forgot to push after airtable issues. sorry!

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -8,44 +8,51 @@ class TabLink {
     this.itemElement = document.querySelector(`.tabs-item[data-tab='${this.data}']`);
     
     // Using the Item element, create a new instance of the TabItem class
-    this.tabItem = new Item(this.tabItem);
+    this.tabItem = new TabItem(this.tabItem);
     
     // Add a click event listener on this instance, calling the select method on click
     this.element.addEventListener('click', () => {
-      this.select(event);
-    })
+      this.select();
+    });
   };
 
   select() {
     // Get all of the elements with the tabs-link class
-    const links;
+    const links = document.querySelectorAll('.tabs-link');
 
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-    Array.from(links).forEach();
+    links.forEach(links => {
+      links.classList.remove('tabs-link-selected')
+    });
 
     // Add a class named "tabs-link-selected" to this link
-    this.element;
+    this.element.classList.add('tabs-link-selected');
     
     // Call the select method on the item associated with this link
-
+    this.tabItem.select();
   }
 }
 
 class TabItem {
   constructor(element) {
-    this.element;
+    this.element = element;
   }
 
   select() {
     // Select all items elements from the DOM
+    const allItems = document.querySelectorAll('.tabs-item')
     // Remove the class "tabs-item-selected" from each element
+    allItems.forEach( item => {
+      item.classList.remove('tabs-item-selected');
+    });
     // Add a class named "tabs-item-selected" to this element 
+    this.element.classList.add('tabs-link-selected');
   }
 }
 
 
 // START HERE: create a reference to the ".tabs" classes
-let links = document.querySelectorAll('.tabs');
+let links = document.querySelectorAll('.tabs-link');
 
 // Following the code in the Dropdown file, iterate through the array you created above creating a new instance of the TabLink class for each item. 
 links = Array.from(links).map(link => {
@@ -53,3 +60,4 @@ links = Array.from(links).map(link => {
 })
 
 // DO THIS LAST: Once you have created an array of TabLink instances. call select() on the first item in the array
+links[0].select();
