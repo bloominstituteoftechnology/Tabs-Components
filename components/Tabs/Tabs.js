cd41bcd421d8714d64ec4ed79b6fae74f5f53cc4
Trai Lynne <trailynne@outlook.com>
2018-10-25 16:04:16 -0600

    Tabs: Removed .tabs-link-selected class from all links in select method

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -22,9 +22,9 @@ class TabLink {
         let links = document.querySelectorAll('.tabs-link');
 
         // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-        // Array.from(links).forEach();
+        Array.from(links).forEach(link => link.classList.remove('tabs-link-selected'));
 
-        // Add a class named "tabs-link-selected" to this link
+        // Add a class named ."tabs-link-selected" to this link
         // this.element;
 
         // Call the select method on the item associated with this link
