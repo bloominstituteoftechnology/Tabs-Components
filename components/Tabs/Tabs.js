bd7ea5345078f80c32e14dd681a20bb7e669ccba
Trai Lynne <trailynne@outlook.com>
2018-10-25 15:37:07 -0600

    Tabs: Iterated through NodeList to create an array of TabLink object The Element nodes are then mapped into an array On each iteration the DOM Element is turned into a TabLink js object

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -48,6 +48,6 @@ let links = document.querySelectorAll('.tabs-link');
 
 
 // Following the code in the Dropdown file, iterate through the array you created above creating a new instance of the TabLink class for each item. 
-links = Array.from(links)
+links = Array.from(links).map(link => new TabLink(link));
 
 // DO THIS LAST: Once you have created an array of TabLink instances. call select() on the first item in the array
