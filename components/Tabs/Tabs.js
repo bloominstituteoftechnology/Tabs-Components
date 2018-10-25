a78eb7bf4d58b7eddf0e0c7b71a971e95a5d2055
Trai Lynne <trailynne@outlook.com>
2018-10-25 15:40:57 -0600

    Tabs: Set TabLink data prop equal to the objects data-tab attr in HTML

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -1,9 +1,11 @@
 class TabLink {
     constructor(element) {
-        this.element;
+        this.element = element;
 
         // Get the custom data attribute on the Link
-        this.data;
+        this.data = this.element.dataset.tab;
+
+        console.log(this.data)
 
         // Using the custom data attribute get the associated Item element
         this.itemElement;
@@ -17,13 +19,13 @@ class TabLink {
 
     select() {
         // Get all of the elements with the tabs-link class
-        const links;
+        // const links;
 
         // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-        Array.from(links).forEach();
+        // Array.from(links).forEach();
 
         // Add a class named "tabs-link-selected" to this link
-        this.element;
+        // this.element;
 
         // Call the select method on the item associated with this link
 
