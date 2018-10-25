ecab24f83877103b39ffc3cec9773458050c6c8f
Tyrone Cartwright <tyrone.cartwright@yahoo.com>
2018-10-25 14:37:36 -0400

    Dropdown completed

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -17,9 +17,8 @@ class Dropdown {
     }
 
     toggleContent() {
-
         // Toggle the ".dropdown-hidden" class off and on
-        this.content;
+        this.content.classList.toggle('dropdown-hidden');
     }
 }
 
