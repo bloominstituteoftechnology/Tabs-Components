f9ff51365cda83d9502f43b268bf3170d3a2212e
Lidii <lidiiany92@gmail.com>
2018-10-25 15:41:34 -0400

    Started to work on the tabs.js file

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -1,16 +1,18 @@
 
 class TabLink {
   constructor(element) {
-    this.element;
+    this.element = element;
     
     // Get the custom data attribute on the Link
-    this.data;
+    this.data = this.element.dataset.tab;
+    console.log(this.data);
     
     // Using the custom data attribute get the associated Item element
-    this.itemElement;
+    this.itemElement = document.querySelector(`.tabs-item[data-tab='${this.data}']`);
+    console.log(this.itemElement);
     
     // Using the Item element, create a new instance of the TabItem class
-    this.tabItem;
+    this.tabItem = new TabItem(this.itemElement);;
     
     // Add a click event listener on this instance, calling the select method on click
 
@@ -18,7 +20,7 @@ class TabLink {
 
   select() {
     // Get all of the elements with the tabs-link class
-    const links;
+    //const links;
 
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
     Array.from(links).forEach();
@@ -45,9 +47,9 @@ class TabItem {
 
 
 // START HERE: create a reference to the ".tabs-link" class
-let links = document.querySelectorAll();
+let links = document.querySelectorAll('.tabs-link');
 
 // Following the code in the Dropdown file, iterate through the array you created above creating a new instance of the TabLink class for each item. 
-links = Array.from(links)
+links = Array.from(links).map( link => new TabLink(link));
 
 // DO THIS LAST: Once you have created an array of TabLink instances. call select() on the first item in the array
