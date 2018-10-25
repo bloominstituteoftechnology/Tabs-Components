6ab74d93a29995a2142239a41b19754ebda1904d
Trai Lynne <trailynne@outlook.com>
2018-10-25 15:54:55 -0600

    Tabs: Added Click Event Handler to TabLink element

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -9,15 +9,18 @@ class TabLink {
         this.itemElement = document.querySelector(`.tabs-item[data-tab= '${this.data}']`);
         // Using the Item element, create a new instance of the TabItem class
         this.tabItem = new TabItem(this.itemElement);
-        console.log(this.tabItem);
 
         // Add a click event listener on this instance, calling the select method on click
+
+        this.element.addEventListener('click', () => this.select());
 
     };
 
     select() {
-        // Get all of the elements with the tabs-link class
-        // const links;
+
+        console.log(`Tab ${this.data} was selected!`)
+            // Get all of the elements with the tabs-link class
+            // const links;
 
         // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
         // Array.from(links).forEach();
