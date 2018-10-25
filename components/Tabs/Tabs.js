175cc2e8279cab54f583bf88e62047157d3bb090
Tyrone Cartwright <tyrone.cartwright@yahoo.com>
2018-10-25 18:02:45 -0400

    quick change

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -6,7 +6,7 @@ class TabLink {
         this.data = this.element.dataset.tab;
 
         // Using the custom data attribute get the associated Item element
-        this.itemElement = document.querySelector(`.tab-item[data-tab='${this.data}']`);
+        this.itemElement = document.querySelector(`.tabs-item[data-tab='${this.data}']`);
 
         // Using the Item element, create a new instance of the TabItem class
         this.tabItem = new TabItem(this.itemElement);
@@ -18,21 +18,26 @@ class TabLink {
     }
 
     select() {
-        // Get all of the elements with the tabs-link class
-        let links = document.querySelectorAll('.tabs-link');
+            // Get all of the elements with the tabs-link class
+            let links = document.querySelectorAll('.tabs-link');
 
-        // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-        Array.from(links).forEach(link => {
-            link.classList.remove('tabs-link-selected')
-                // console.log(link);
-        });
+            // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
+            Array.from(links).forEach(link => {
+                link.classList.remove('tabs-link-selected')
+                    // console.log(link);
+            });
 
-        // Add a class named "tabs-link-selected" to this link
-        this.element.classList.add('tabs-link-selected');
+            // Add a class named "tabs-link-selected" to this link
+            this.element.classList.add('tabs-link-selected');
 
-        // Call the select method on the item associated with this link
-        this.tabItem.select();
-    }
+            // Call the select method on the item associated with this link
+            this.tabItem.select();
+        }
+        // deselect() {
+        //     this.link.classList.remove('tabs-link-selected');
+
+    //     this.tabItem.deselect();
+    // }
 }
 
 class TabItem {
@@ -40,17 +45,20 @@ class TabItem {
         this.element = element;
     }
     select() {
-        // Select all ".tabs-item" elements from the DOM
-        let items = document.querySelectorAll('.tabs-item');
-        // Remove the class "tabs-item-selected" from each element
-        Array.from(items).forEach(item => {
-            item.classList.remove('tabs-item-selected');
-            // console.log(items);
-        });
-        // Add a class named "tabs-item-selected" to this element 
-        this.element.classList.add('tabs-item-selected');
-        console.log(this.element);
-    }
+            // Select all ".tabs-item" elements from the DOM
+            let items = document.querySelectorAll('.tabs-item');
+            // Remove the class "tabs-item-selected" from each element
+            Array.from(items).forEach(item => {
+                item.classList.remove('tabs-item-selected');
+                // console.log(items);
+            });
+            // Add a class named "tabs-item-selected" to this element 
+            this.element.classList.add('tabs-item-selected');
+            console.log(this.element);
+        }
+        // deselect() {
+        //     this.link.classList.remove('tabs-link-selected');
+        // }
 }
 
 
