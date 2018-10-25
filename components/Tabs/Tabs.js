f1976204445702ddffbc01942655bba42d34e835
Tyrone Cartwright <tyrone.cartwright@yahoo.com>
2018-10-25 18:22:07 -0400

    no change

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -19,7 +19,7 @@ class TabLink {
 
     select() {
             // Get all of the elements with the tabs-link class
-            let links = document.querySelectorAll('.tabs-link');
+            const links = document.querySelectorAll('.tabs-link');
 
             // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
             Array.from(links).forEach(link => {
