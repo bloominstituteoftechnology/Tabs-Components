a9b8527f64f236b259bbb0360d3046a55da1c2fc
Trai Lynne <trailynne@outlook.com>
2018-10-25 16:48:17 -0600

    Stretch: Removed reverences to tab links at bottom of page

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -73,14 +73,14 @@ class TabItem {
 let tabPanel = document.querySelectorAll('.tabs-links')
 
 
-let links = document.querySelectorAll('.tabs-link');
+// let links = document.querySelectorAll('.tabs-link');
 
 
 // Following the code in the Dropdown file, iterate through the array you created above creating a new instance of the TabLink class for each item. 
-links = Array.from(links).map(link => new TabLink(link));
+// links = Array.from(links).map(link => new TabLink(link));
 
 tabPanel = Array.from(tabPanel).map(panel => new Tabs(panel));
 
 // DO THIS LAST: Once you have created an array of TabLink instances. call select() on the first item in the array
 
-links[0].select();
+// links[0].select();
