51e66810a1e35f14db4a8996af630ba619352f0c
Trai Lynne <trailynne@outlook.com>
2018-10-25 15:10:30 -0600

    Dropdown: Wrote toggleContent methodto toggle .dropdown-hidden class

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -13,13 +13,13 @@ class Dropdown {
         // Add a click handler to the button reference and call the toggleContent method.
         this.button.addEventListener('click', () => {
             this.toggleContent();
-        })
+        });
     }
 
     toggleContent() {
 
         // Toggle the ".dropdown-hidden" class off and on
-        console.log(this.content);
+        this.content.classList.toggle('dropdown-hidden');
     }
 }
 
