7b7466cfc50b07c981ff5ab8623ea755b1998be2
Trai Lynne <trailynne@outlook.com>
2018-10-25 16:00:15 -0600

    Tabs: collected fresh NodeList of links in select() method

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -18,9 +18,8 @@ class TabLink {
 
     select() {
 
-        console.log(`Tab ${this.data} was selected!`)
-            // Get all of the elements with the tabs-link class
-            // const links;
+        // Get all of the elements with the tabs-link class
+        let links = document.querySelectorAll('.tabs-link');
 
         // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
         // Array.from(links).forEach();
@@ -29,6 +28,8 @@ class TabLink {
         // this.element;
 
         // Call the select method on the item associated with this link
+
+        console.log(links);
 
     }
 }
