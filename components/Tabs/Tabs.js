9a4ef2203c27f16f4dedb6315fcb99e494ed0fdd
brooks <bpoltl1@gmail.com>
2018-10-25 17:07:21 -0500

    added animation to beginning boxes

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -20,11 +20,9 @@ class TabLink {
     // Get all of the elements with the tabs-link class
     const links = document.querySelectorAll('.tabs-link');
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-    Array.from(links).forEach(links => links.classList.remove('tabs-linkselected'));
+    Array.from(links).forEach(links => links.classList.remove('.tabs-link-selected'));
     // Add a class named "tabs-link-selected" to this link
     this.element.classList.add('.tabs-link-selected')
-    
-    
     // Call the select method on the item associated with this link
     this.itemElement.select();
   }
