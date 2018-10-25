b9d02ac5364f3b28e8967d28d07d9766e1e0e46d
dyiar <austin.hl.blake@gmail.com>
2018-10-25 16:28:54 -0600

    Added a small animation to the tab item openeing

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -1,3 +1,34 @@
+// class Tabs {
+//   constructor(element) {
+//     this.element = element;
+
+//     this.data = this.element.dataset.tab;
+//     this.tabLink = document.querySelector(`.tabs-link[data-tab='${this.data}']`);
+//     this.tabLinks = new TabLink (tabLink);
+//     // this.itemElement = document.querySelector(`.tabs-item[data-tab='${this.data}']`);
+//     // this.tabItem = new TabItem (this.itemElement);
+//     // this.element.addEventListener('click', () => { this.select() });
+
+    
+//   };
+
+//   select(){ 
+//     // Get all of the elements with the tabs-link class
+//     // const links = document.querySelectorAll('.tabs-link');
+
+//     // // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
+//     // Array.from(links).forEach(function(element) {
+//     // // console.log("hello");
+//     // element.classList.remove('tabs-link-selected');
+//     // });
+  
+//     // // Add a class named "tabs-link-selected" to this link
+//     // this.element.classList.add('tabs-link-selected');
+//     this.tabLink.select();
+
+//   }
+// }
+
 
 class TabLink {
   constructor(element) {
@@ -30,6 +61,7 @@ class TabLink {
     // Add a class named "tabs-link-selected" to this link
     this.element.classList.add('tabs-link-selected');
     
+    
     // Call the select method on the item associated with this link
     this.tabItem.select();
 
@@ -50,25 +82,12 @@ class TabItem {
     Array.from(items).forEach(function(element) {
       element.classList.remove('tabs-item-selected');
     });
-    console.log('hello')
+    // console.log('hello')
 
-    this.element.classList.add('tabs-item-selected');
+    // this.element.classList.add('tabs-item-selected');
+    TweenLite.to(this.element, .5, {className: "+=tabs-item-selected", ease:Power1.easeOut});
   }
 }
-
-// class Tabs {
-//   constructor(element) {
-//     this.element = element;
-
-//     this.data = this.element.dataset.tab;
-//     this.itemElement = document.querySelector(`.tabs-item[data-tab='${this.data}']`);
-//     this.tabItem = new TabItem (this.itemElement);
-//     this.element.addEventListener('click', () => { this.select() });
-
-//     this.tabLink = 
-//     this.tabLinks = new TabLink (tabLink);
-//   }
-// }
 
 
 // START HERE: create a reference to the ".tabs-link" class
