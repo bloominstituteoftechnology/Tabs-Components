75bc21ebb2cf00e2355fcb812ee0e7cb22c6c9f2
Trai Lynne <trailynne@outlook.com>
2018-10-25 15:51:52 -0600

    Tabs: Created a new TabItem object out of the TabLinks itemElement

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -7,9 +7,9 @@ class TabLink {
 
         // Using the custom data attribute get the associated Item element
         this.itemElement = document.querySelector(`.tabs-item[data-tab= '${this.data}']`);
-        console.log(this.itemElement)
-            // Using the Item element, create a new instance of the TabItem class
-        this.tabItem;
+        // Using the Item element, create a new instance of the TabItem class
+        this.tabItem = new TabItem(this.itemElement);
+        console.log(this.tabItem);
 
         // Add a click event listener on this instance, calling the select method on click
 
