a9e3f97245d4b2e4aa338a44ec6aea6f1f673533
Lidii <lidiiany92@gmail.com>
2018-10-25 16:58:55 -0400

    Finished tabs.js

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -12,36 +12,42 @@ class TabLink {
     console.log(this.itemElement);
     
     // Using the Item element, create a new instance of the TabItem class
-    this.tabItem = new TabItem(this.itemElement);;
+    this.tabItem = new TabItem(this.itemElement);
+    console.log(this.tabItem);
     
     // Add a click event listener on this instance, calling the select method on click
+    this.element.addEventListener('click', () => { this.select() }); 
 
   };
 
   select() {
     // Get all of the elements with the tabs-link class
-    //const links;
+    const links = document.querySelectorAll('.tabs-link');
 
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-    Array.from(links).forEach();
+    Array.from(links).forEach(link => link.classList.remove('tabs-link-selected'));
 
     // Add a class named "tabs-link-selected" to this link
-    this.element;
+    this.element.classList.add('tabs-link-selected');
     
     // Call the select method on the item associated with this link
-
+      this.tabItem.select();
   }
 }
 
 class TabItem {
   constructor(element) {
-    this.element;
+    this.element = element;
   }
 
   select() {
     // Select all ".tabs-item" elements from the DOM
+    const tabsItems = document.querySelectorAll('.tabs-item');
     // Remove the class "tabs-item-selected" from each element
+    Array.from(tabsItems).forEach(item => item.classList.remove('tabs-item-selected'));
     // Add a class named "tabs-item-selected" to this element 
+    this.element.classList.add('tabs-item-selected');
+
   }
 }
 
@@ -53,3 +59,4 @@ let links = document.querySelectorAll('.tabs-link');
 links = Array.from(links).map( link => new TabLink(link));
 
 // DO THIS LAST: Once you have created an array of TabLink instances. call select() on the first item in the array
+links[0].select();
