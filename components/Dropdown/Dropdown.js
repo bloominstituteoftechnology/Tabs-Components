5332908bb5c91687b45abdfcb439887820e449d8
Josh Z <jzieger10@gmail.com>
2018-10-25 20:22:47 -0400

    Noticed a mistake that prevented the articles from showing properly. Corrected and working now.

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -19,7 +19,6 @@ class Dropdown {
   toggleContent() {
     // Toggle the ".dropdown-hidden" class off and on
     this.content.classList.toggle('dropdown-hidden');
-    console.log('clicked');
   }
 }
 
