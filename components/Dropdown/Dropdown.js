1063d98b8aed102ad3f473cb81d16ad217417859
Joseph Eastman <josephryan96@gmail.com>
2018-11-01 16:51:48 -0700

    Dropdown.js toggleContent fix

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -1,36 +1,54 @@
 class Dropdown {
   constructor(element) {
-    
     // Assign this.element to the dropdown element
     this.element = element;
-    
+
     // Get the element with the ".dropdown-button" class found in the dropdown element (look at the HTML for context)
-    this.button = this.element.querySelector('.dropdown-button');
-    
+    this.button = this.element.querySelector(".dropdown-button");
+
     // assign the reference to the ".dropdown-content" class found in the dropdown element
-    this.content = this.element.querySelector('.dropdown-content');
-    
+    this.content = this.element.querySelector(".dropdown-content");
+
     // Add a click handler to the button reference and call the toggleContent method.
-    this.button.addEventListener('click', () => this.toggleContent());
+    this.button.addEventListener("click", () => this.toggleContent());
   }
 
   toggleContent() {
-    
     // Toggle the ".dropdown-hidden" class off and on
     // this.content.classList.toggle('dropdown-hidden');
     // TweenMax.fromTo(this.content, 1, {x: -200}, {left: 200}, {ease: Power4.easeIn});
 
-    if (this.content.classList.contains('dropdown-hidden')) {
-      TweenMax.fromTo(this.content, 1, {x: -200}, {left: 200}, {ease: Power4.easeIn});
-      this.content.classList.remove('dropdown-hidden');
-    } else {TweenMax.fromTo(this.content, 1, {x: -200}, {left: 0}, {ease: Power4.easeIn}); onCompleteParam: () => this.content.classList.add('dropdown-hidden')}
+    if (this.content.classList.contains("dropdown-hidden")) {
+      TweenMax.fromTo(
+        this.content,
+        1,
+        { x: -200 },
+        { x: 0 },
+        { ease: Power4.easeIn }
+      );
+      this.content.classList.remove("dropdown-hidden");
+    } else {
+      TweenMax.fromTo(
+        this.content,
+        1,
+        { x: 0 },
+        {
+          x: -200,
+          ease: Power4.easeIn,
+          onComplete: () => this.content.classList.add("dropdown-hidden")
+        }
+      );
+    }
   }
 }
 
 // Nothing to do here, just study what the code is doing and move on to the Dropdown class
-let dropdowns = document.querySelectorAll('.dropdown');
-dropdowns = Array.from(dropdowns).map( dropdown => new Dropdown(dropdown));
+let dropdowns = document.querySelectorAll(".dropdown");
+dropdowns = Array.from(dropdowns).map(dropdown => new Dropdown(dropdown));
 
-let body = document.querySelector('body');
+let body = document.querySelector("body");
 
-body.addEventListener('dblclick', () => document.body.style.backgroundImage = "url('/img/background-img.jpg')");
+body.addEventListener(
+  "dblclick",
+  () => (document.body.style.backgroundImage = "url('/img/background-img.jpg')")
+);
