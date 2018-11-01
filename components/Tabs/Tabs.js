305ca04054079cb89185fb5bd3418dc989a722e6
Rachel Jefferies <rachel_lynne_t@yahoo.com>
2018-11-01 18:04:03 -0400

    working on first select method, obvious bugs

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -14,17 +14,21 @@ class TabLink {
     
     // Add a click event listener on this instance, calling the select method on click
 
+    this.link.addEventListener('click', () => { this.select()
+    
+    }); 
+
   };
 
   select() {
     // Get all of the elements with the tabs-link class
-    // const links;
+    const links = document.querySelector(".tabs-link");
 
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-    Array.from(links).forEach();
+    Array.from(links).forEach(element => new Tablink(element));
 
     // Add a class named "tabs-link-selected" to this link
-    this.element;
+    this.element =this.dataset.tabs-link-selected;
     
     // Call the select method on the item associated with this link
 
@@ -33,7 +37,7 @@ class TabLink {
 
 class TabItem {
   constructor(element) {
-    this.element;
+    this.element= element;
   }
 
   select() {
@@ -48,6 +52,6 @@ class TabItem {
 let links = document.querySelectorAll(".tabs-links");
 
 // Following the code in the Dropdown file, iterate through the array you created above creating a new instance of the TabLink class for each item. 
-links = Array.from(links).map( element => new Tablink(element));
+links = Array.from(links).map( element => new TabLink(element));
 
 // DO THIS LAST: Once you have created an array of TabLink instances. call select() on the first item in the array
