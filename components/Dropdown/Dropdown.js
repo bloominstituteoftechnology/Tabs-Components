a4c8fea82a709f91f47ef3b977d73c63568c794b
chrisgins <christiansgina@gmail.com>
2018-11-01 11:11:43 -0600

    Toggle the ".dropdown-hidden" class off and on

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -20,7 +20,7 @@ class Dropdown {
   toggleContent() {
     
     // Toggle the ".dropdown-hidden" class off and on
-    this.content;
+    this.content.classList.toggle('dropdown-hidden');
   }
 }
 
