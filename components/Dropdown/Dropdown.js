6cabc27703583b27cb0748ee9c41dcb3f90398cc
Rachel Jefferies <rachel_lynne_t@yahoo.com>
2018-11-01 20:32:24 -0400

    Added a tab

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -8,15 +8,17 @@ class Dropdown {
     this.button = this.element.querySelector(".dropdown-button");
     
     // assign the reference to the ".dropdown-content" class found in the dropdown element
-    this.content = document.querySelector(".dropdown-content");
+    // this.content = document.querySelector(".dropdown-content");
+    this.content = this.element.querySelector(".dropdown-content");
     
     // Add a click handler to the button reference and call the toggleContent method.
-    this.button.addEventListener('click', () => this.toggleContent());
-  }
-
-  toggleContent() {
+    // Read --->Instructor note, the reason we must wrap the toggleContent method in an anonymous function is that 'this' in toggleContent
+    // would refer to the button, NOT the current instance of the class. 
+    this.button.addEventListener('click', () => { this.toggleContent() })
     
     // Toggle the ".dropdown-hidden" class off and on
+    //this commented out code below was wrong. Pay attention to what you are selecting.
+    // this.element.classList.toggle('dropdown-hidden');
     this.content.classList.toggle('dropdown-hidden');
   }
 }
@@ -24,3 +26,4 @@ class Dropdown {
 // Nothing to do here, just study what the code is doing and move on to the Dropdown class
 let dropdowns = document.querySelectorAll('.dropdown');
 dropdowns = Array.from(dropdowns).map( dropdown => new Dropdown(dropdown));
+
