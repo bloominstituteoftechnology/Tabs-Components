b401908ee5de75c9b49279d776d52534ae57d291
Josh Knell <bigknell@gmail.com>
2018-11-05 20:29:27 -0700

    Updated instructions to now use forEach instead

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -1,16 +1,17 @@
 
 class TabLink {
   constructor(element) {
-    this.element;
+    // Assign this.element to the passed in DOM element
+    // this.element;
     
     // Get the custom data attribute on the Link
-    this.data;
+    // this.data;
     
     // Using the custom data attribute get the associated Item element
-    this.itemElement;
+    // this.itemElement;
     
     // Using the Item element, create a new instance of the TabItem class
-    this.tabItem;
+    // this.tabItem;
     
     // Add a click event listener on this instance, calling the select method on click
 
@@ -18,13 +19,13 @@ class TabLink {
 
   select() {
     // Get all of the elements with the tabs-link class
-    const links;
+    // const links;
 
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-    Array.from(links).forEach();
+    // Array.from(links).forEach();
 
     // Add a class named "tabs-link-selected" to this link
-    this.element;
+    // this.element;
     
     // Call the select method on the item associated with this link
 
@@ -33,21 +34,29 @@ class TabLink {
 
 class TabItem {
   constructor(element) {
-    this.element;
+    // Assign this.element to the passed in element
+    // this.element;
   }
 
   select() {
     // Select all ".tabs-item" elements from the DOM
+    // const items;
+
     // Remove the class "tabs-item-selected" from each element
-    // Add a class named "tabs-item-selected" to this element 
+    
+    // Add a class named "tabs-item-selected" to this element
+    //this.element;
   }
 }
 
+/* START HERE: 
 
-// START HERE: create a reference to the ".tabs-link" class
-let links = document.querySelectorAll();
+- Select all classes named ".tabs-link" and assign that value to the links variable
 
-// Following the code in the Dropdown file, iterate through the array you created above creating a new instance of the TabLink class for each item. 
-links = Array.from(links)
+- With your selection in place, now chain a .forEach() method onto the links variable to iterate over the DOM NodeList
 
-// DO THIS LAST: Once you have created an array of TabLink instances. call select() on the first item in the array
+- In your .forEach() method's callback function, return a new instance of TabLink and pass in each link as a parameter
+
+*/
+
+links = document.querySelectorAll();
