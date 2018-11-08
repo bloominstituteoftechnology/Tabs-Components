9b2b9fc2cd80d7e2124e0b0778681cf0f2080b7b
Noah <noah.stoeckel@gmail.com>
2018-11-07 23:31:01 -0500

    Complete Part One

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -2,27 +2,34 @@ class Dropdown {
   constructor(element) {
     
     // Assign this.element to the dropdown element
-    this.element;
+    this.element = element;
     
     // Get the element with the ".dropdown-button" class found in the dropdown element (look at the HTML for context)
-    this.button = this.element.querySelector();
+    this.button = this.element.querySelector('.dropdown-button');
     
     // assign the reference to the ".dropdown-content" class found in the dropdown element
-    this.content;
+    this.content= this.element.querySelector('.dropdown-content');
     
     // Add a click handler to the button reference and call the toggleContent method.
     this.button.addEventListener('click', () => {
-
+      this.toggleContent()  
     })
   }
 
   toggleContent() {
     
     // Toggle the ".dropdown-hidden" class off and on
-    this.content;
+    this.content.classList.toggle('dropdown-hidden');
   }
 }
 
 
 // Nothing to do here, just study what the code is doing and move on to the Dropdown class
 let dropdowns = document.querySelectorAll('.dropdown').forEach( dropdown => new Dropdown(dropdown));
+// it's interesting here that  by default  this is query selector all
+// which means no matter how many of these components on the pages there are
+// all of them will work as expected.
+
+//However I've read, that after certain point adding event listeners can affect performance
+//so I am curious as to what that limit is, and if you can write classes in such a way
+//that is more similar to old school event delegation
