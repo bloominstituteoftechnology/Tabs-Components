4d18fee1b27aabb6bf421fedb774fa78c9260ab0
jrizza88 <jetorres88@gmail.com>
2018-11-08 20:44:41 -0500

    got the custom data attribute to work with the associated item element

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -9,8 +9,8 @@ class TabLink {
     console.log(this.data)
     
     // Using the custom data attribute get the associated Item element
-    // this.itemElement;
-    
+    this.itemElement = document.querySelector(`.tabs-item[data-tab='${this.data}']`)
+    console.log(this.itemElement)
     // Using the Item element, create a new instance of the TabItem class
     // this.tabItem;
     
