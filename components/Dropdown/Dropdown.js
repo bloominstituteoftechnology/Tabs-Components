3ae6fcae7f52daf0a92fbb788dc77bf34dda9e26
dickwinters <coding1965@gmail.com>
2018-11-08 22:42:57 -0600

    completed dropdown animation of menu

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -1,22 +1,32 @@
 class Dropdown {
   constructor(element) {
+    let counter = 1;
     // Assign this.element to the dropdown element
     this.element = element;
 
     // Get the element with the ".dropdown-button" class found in the dropdown element (look at the HTML for context)
     this.button = this.element.querySelector('.dropdown-button');
-
     // assign the reference to the ".dropdown-content" class found in the dropdown element
     this.content = this.element.querySelector('.dropdown-content');
-    // Add a click handler to the button reference and call the toggleContent method.
-    this.button.addEventListener('click', () => {
-      this.toggleContent();
-    });
-  }
 
-  toggleContent() {
-    // Toggle the ".dropdown-hidden" class off and on
-    this.content.classList.toggle('dropdown-hidden');
+    this.button.addEventListener('click', () => {
+      TweenLite.fromTo(
+        this.content,
+        1,
+        {width: 126, height: 0},
+        {width: 126, height: 158},
+      );
+    });
+    // Add a click handler to the button reference and call the toggleContent method.
+    //   this.button.addEventListener('click', () => {
+    //     this.toggleContent();
+    //   });
+    // }
+    //
+    // toggleContent() {
+    //   // Toggle the ".dropdown-hidden" class off and on
+    //   this.content.classList.toggle('dropdown-hidden');
+    // }
   }
 }
 
