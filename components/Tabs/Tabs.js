a62419cdd0bc597679d332c28185133bd2e6c4cc
amadison17 <angelmadison2015@gmail.com>
2018-11-15 20:44:36 -0500

    Tabs.js

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -4,30 +4,35 @@ class TabLink {
     this.element;
     
     // Get the custom data attribute on the Link
-    this.data;
+    this.data = this.element.dataset.tabs
     
     // Using the custom data attribute get the associated Item element
-    this.itemElement;
+    this.itemElement = document.querySelector('.tabs-items');
     
     // Using the Item element, create a new instance of the TabItem class
-    this.tabItem;
+    this.tabItem = new TabItem(element);
     
     // Add a click event listener on this instance, calling the select method on click
-
-  };
+    this.tabItem.addEventListener('click', () => {
+      
+      this.select()
+    });
+  
 
   select() {
     // Get all of the elements with the tabs-link class
-    const links;
+    const links = document.querySelectorAll(".tab-links");
 
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-    Array.from(links).forEach();
+    Array.from(links).forEach(function(item){
+      links.classList.remove('.tabs-link-selected')
+    });
 
     // Add a class named "tabs-link-selected" to this link
-    this.element;
+    this.element = document.querySelectorAll('.tabs-link-selected');
     
     // Call the select method on the item associated with this link
-
+    select(element);
   }
 }
 
@@ -45,9 +50,9 @@ class TabItem {
 
 
 // START HERE: create a reference to the ".tabs-link" class
-let links = document.querySelectorAll();
+let links = document.querySelectorAll(".tab-links");
 
 // Following the code in the Dropdown file, iterate through the array you created above creating a new instance of the TabLink class for each item. 
-links = Array.from(links)
+links = Array.from(links).map(links => new TabLink(links));
 
 // DO THIS LAST: Once you have created an array of TabLink instances. call select() on the first item in the array
