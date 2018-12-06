e1fc179d8f7faabcd0bd50db4d304a01dc1a4669
rosilioroman <rosilioroman@gmail.com>
2018-12-06 16:31:36 -0700

    Experimented with animations. Only implemented a Focus-In CSS animatio on the TabItem text when the TabLink is clicked.

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -7,9 +7,7 @@ class Dropdown {
     // assign the reference to the ".dropdown-content" class found in the dropdown element
     this.content = this.element.querySelector('.dropdown-content');
     // Add a click handler to the button reference and call the toggleContent method.
-    this.button.addEventListener('click', () => {
-      return this.toggleContent();
-    })
+    this.button.addEventListener('click', () => this.toggleContent());
   }
   toggleContent() {
     // Toggle the ".dropdown-hidden" class off and on
