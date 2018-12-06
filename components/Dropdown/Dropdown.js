29a14428b0fcab91b641dd2ab118469dc88d4db2
ajfrierson <alvinfrierson@fuller.edu>
2018-12-06 12:41:13 -0800

    working on dropdown.js

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -1,28 +1,29 @@
 class Dropdown {
-  constructor(element) {
+  constructor(link) {
     
     // Assign this.element to the dropdown element
-    this.element;
+    this.link = link;
     
     // Get the element with the ".dropdown-button" class found in the dropdown element (look at the HTML for context)
-    this.button = this.element.querySelector();
+    this.button = document.querySelector(`.dropdown-button[data-tab="${this.linkData}"]`);
     
     // assign the reference to the ".dropdown-content" class found in the dropdown element
-    this.content;
+    this.content = this.link.dataset.tab;
     
     // Add a click handler to the button reference and call the toggleContent method.
-    this.button.addEventListener('click', () => {
+    this.button.addEventListener('click', () => {this.toggleContent()});
 
-    })
   }
 
   toggleContent() {
     
     // Toggle the ".dropdown-hidden" class off and on
-    this.content;
+    this.content.classList.toggle('dropdown-open');
   }
 }
 
 
+
 // Nothing to do here, just study what the code is doing and move on to the Dropdown class
 let dropdowns = document.querySelectorAll('.dropdown').forEach( dropdown => new Dropdown(dropdown));
+
