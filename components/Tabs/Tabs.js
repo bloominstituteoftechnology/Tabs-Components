b7b0dbbd132b2a27e4c29ad2519ca4149a741664
BrannanC <brannanconrad@gmail.com>
2018-12-06 15:55:24 -0600

    Sliding opacity tabs

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -1,4 +1,3 @@
-
 class TabLink {
   constructor(element) {
     // Assign this.element to the passed in DOM element
@@ -45,20 +44,22 @@ class TabItem {
     // Remove the class "tabs-item-selected" from each element
     items.forEach(el => el.classList.remove('tabs-item-selected'));
     // Add a class named "tabs-item-selected" to this element
+    TweenMax.fromTo(this.element, 1, {
+      opacity: 0,
+      x: 200
+    }, {
+      opacity: 1,
+      x: 0
+    });
     this.element.classList.add('tabs-item-selected');
   }
 }
 
 /* START HERE: 
-
 - Select all classes named ".tabs-link" and assign that value to the links variable
-
 - With your selection in place, now chain a .forEach() method onto the links variable to iterate over the DOM NodeList
-
 - In your .forEach() method's callback function, return a new instance of TabLink and pass in each link as a parameter
-
 */
 
 links = document.querySelectorAll('.tabs-link');
 links.forEach(link => link = new TabLink(link));
-console.log(links);
