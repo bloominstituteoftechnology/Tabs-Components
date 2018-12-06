b1d365b0a6ec3dda37eec945b4b7c26adc2377c1
croy <roy.christie95@gmail.com>
2018-12-06 16:24:00 -0500

    Fixed dropdown to actually work

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -5,14 +5,16 @@ class Dropdown {
     // Get the element with the ".dropdown-button" class found in the dropdown element (look at the HTML for context)
     this.button = this.element.querySelector('.dropdown-button');
     // assign the reference to the ".dropdown-content" class found in the dropdown element
-    this.content = this.element.querySelector('dropdown-content');
+    this.content = this.element.querySelector('.dropdown-content');
     // Add a click handler to the button reference and call the toggleContent method.
-    this.button.addEventListener('click', (event) => {this.toggleContent(event)});
+    this.button.addEventListener('click', e => {
+      this.toggleContent(e)
+    })
   }
 
-  toggleContent(event) {
+  toggleContent(e) {
     // Toggle the ".dropdown-hidden" class off and on
-    this.content.classList.toggleContent('.dropdown-hidden');
+    this.content.classList.toggle('dropdown-hidden');
   }
 }
 
