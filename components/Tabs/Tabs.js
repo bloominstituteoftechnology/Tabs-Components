7c80f306285025fdd9274e839bcd6c10366a55ab
Nathan Thomas <nwthomas@me.com>
2018-12-06 12:45:33 -0800

    Add dropdown menu functionality

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -1,34 +1,24 @@
-
 class TabLink {
   constructor(element) {
     // Assign this.element to the passed in DOM element
     // this.element;
-    
     // Get the custom data attribute on the Link
     // this.data;
-    
     // Using the custom data attribute get the associated Item element
     // this.itemElement;
-    
     // Using the Item element, create a new instance of the TabItem class
     // this.tabItem;
-    
     // Add a click event listener on this instance, calling the select method on click
-
-  };
+  }
 
   select() {
     // Get all of the elements with the tabs-link class
     // const links;
-
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
     // Array.from(links).forEach();
-
     // Add a class named "tabs-link-selected" to this link
     // this.element;
-    
     // Call the select method on the item associated with this link
-
   }
 }
 
@@ -41,9 +31,7 @@ class TabItem {
   select() {
     // Select all ".tabs-item" elements from the DOM
     // const items;
-
     // Remove the class "tabs-item-selected" from each element
-    
     // Add a class named "tabs-item-selected" to this element
     //this.element;
   }
@@ -59,4 +47,4 @@ class TabItem {
 
 */
 
-links = document.querySelectorAll();
+// links = document.querySelectorAll();
