9e2033589fce1726a42b73d57b77b52a23ab960a
KieranVieira <kieranvieira@live.com>
2018-12-06 18:57:13 -0500

    Finishing up box carousel, almost complete but still working on it

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -14,11 +14,11 @@ class Dropdown {
       // this.content.classList.toggle('dropdown-hidden');
       dropActive = true;
       console.log(dropActive);
-      TweenMax.to(".dropdown-content", .5, {top:55})
+      TweenMax.to(".dropdown-content", .5, {top:55, opacity:1})
     }else if(dropActive){
       dropActive = false;
       console.log(dropActive);
-      TweenMax.to(".dropdown-content", .2, {top:-170})
+      TweenMax.to(".dropdown-content", .5, {top:-170, opacity:0})
     }
 
   }
