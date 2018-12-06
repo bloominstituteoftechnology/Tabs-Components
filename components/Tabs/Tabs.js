fdd77fe60a2389a7fe66fa60a3d89945ae8a2bd4
Daniel Weinman <namniewd@gmail.com>
2018-12-06 15:42:51 -0500

    added 2 tabs, main objectives complete

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -21,7 +21,6 @@ class TabLink {
   select() {
     // Get all of the elements with the tabs-link class
     const links = document.querySelectorAll('.tabs-link');
-    console.log(links);
 
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
     // Array.from(links).forEach();
