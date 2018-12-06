5ce8a5e4ad6dcbfd7664eabf0b537e7921ffb8c9
Nathan Thomas <nwthomas@me.com>
2018-12-06 13:29:44 -0800

    Finish .select() method in TabLink class

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -17,16 +17,21 @@ class TabLink {
     this.itemElement = new TabItem(this.itemElement);
 
     // Add a click event listener on this instance, calling the select method on click
+    this.element.addEventListener("click", () => this.select());
   }
 
   select() {
     // Get all of the elements with the tabs-link class
-    // const links;
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-    // Array.from(links).forEach();
+    const links = document
+      .querySelectorAll(".tabs-link")
+      .forEach(tab => tab.classList.remove("tabs-link-selected")); // Array.from(links).forEach();
+
     // Add a class named "tabs-link-selected" to this link
-    // this.element;
+    this.element.classList.toggle("tabs-link-selected");
+
     // Call the select method on the item associated with this link
+    this.itemElement.select();
   }
 }
 
@@ -34,7 +39,6 @@ class TabItem {
   constructor(element) {
     // Assign this.element to the passed in element
     this.element = element;
-    console.log(this.element);
   }
 
   select() {
