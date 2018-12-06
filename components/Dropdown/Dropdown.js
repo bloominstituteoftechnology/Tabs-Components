9ea7acb2749052b219a70be83a8350f85d4a50c8
Imon Ovbude <imonovbude@gmail.com>
2018-12-06 12:34:02 -0800

    added classList toggle to toggleContent method

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -16,7 +16,7 @@ class Dropdown {
 
 	toggleContent() {
 		// Toggle the ".dropdown-hidden" class off and on
-		// this.content.classList.toggle('dropdown-hidden');
+		this.content.classList.toggle('dropdown-hidden');
 	}
 }
 
