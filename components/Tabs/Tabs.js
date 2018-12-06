b3f50fb5b1bd65c5cb7c099f99f2e5f619196880
Samsoucy <newf930@outlook.com>
2018-12-06 18:52:58 -0500

    tabs update

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -13,24 +13,27 @@ class TabLink {
     
     // Using the Item element, create a new instance of the TabItem class
     // this.tabItem;
-    this.itemElement = new TabItem(this.itemElement);
+    this.tabItem = new TabItem(this.itemElement);
     
     // Add a click event listener on this instance, calling the select method on click
-    this.element.addEventListener("click", () => {this.select()})
+    this.element.addEventListener("click", () => this.select());
   };
 
   select() {
     // Get all of the elements with the tabs-link class
     // const links;
+    const links = document.querySelectorAll(".tabs-link")
 
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
     // Array.from(links).forEach();
+    Array.from(links).forEach(item => item.classList.remove("tabs-link-selected"));
+   
 
     // Add a class named "tabs-link-selected" to this link
     // this.element;
-    
+    this.element.classList.add("tabs-link-selected");
     // Call the select method on the item associated with this link
-
+    this.tabItem.select();
   }
 }
 
@@ -44,12 +47,14 @@ class TabItem {
   select() {
     // Select all ".tabs-item" elements from the DOM
     // const items;
+    const items = document.querySelectorAll(".tabs-item");
 
     // Remove the class "tabs-item-selected" from each element
+    Array.from(items).forEach(item => item.classList.remove("tabs-item-selected"));
     
     // Add a class named "tabs-item-selected" to this element
     //this.element;
-    this.element.classList.toggle("tabs-item-selected");
+    this.element.classList.add("tabs-item-selected");
   }
 }
 
@@ -63,4 +68,5 @@ class TabItem {
 
 */
 
-const links = document.querySelectorAll(".tabs-link").forEach(element => new TabLink(element));
+links = document.querySelectorAll(".tabs-link").forEach(link => new TabLink(link));
+
