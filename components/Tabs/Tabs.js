1cb4e25ccf87c764bc5a62cfa80b67bc776ef4d4
JamesB <jblazer1@yahoo.com>
2018-12-06 13:47:26 -0800

    Functioning Dropdown

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -31,7 +31,22 @@ class TabLink {
 
   }
 }
+// =========================== suggested code from Abdul ============================================
+// select() {
+//   // Get all of the elements with the tabs-link class
+//   const links = document.querySelectorAll('.tabs-link');
 
+//   // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
+//   Array.from(links).forEach(link => link.classList.remove('tabs-link-selected'));
+
+//   // // Add a class named "tabs-link-selected" to this link
+//   this.element.classList.add('tabs-link-selected');
+  
+//   // // Call the select method on the item associated with this link
+//   this.tabItem.select();
+// }
+// }
+// ===================================================================================================
 class TabItem {
   constructor(element) {
     // Assign this.element to the passed in element
