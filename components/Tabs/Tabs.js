8a0b497ae2239643e697b76bc04cb5ec10ac6e74
rmejia-tajc <rmejia.tajc@gmail.com>
2018-12-06 16:09:18 -0700

    create TabItem method

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -43,9 +43,10 @@ class TabItem {
     const items = document.querySelectorAll(".tabs-item");
 
     // Remove the class "tabs-item-selected" from each element
-    
+    Array.from(items).forEach(item => item.classList.remove('tabs-item-selected')); 
+
     // Add a class named "tabs-item-selected" to this element
-    //this.element;
+    this.element.classList.add("tabs-item-selected");
   }
 }
 
