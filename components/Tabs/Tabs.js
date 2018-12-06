0cd3c93e6d6d3e5194cf68f0cee39d862f34e15a
ills5771 <ilyayelizarov1@gmail.com>
2018-12-06 13:38:33 -0800

    Added select method and class TabItem

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -22,25 +22,25 @@ class TabLink {
      const links = document.querySelectorAll('.tabs-link');
 
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-    // Array.from(links).forEach();
+    Array.from(links).forEach(link => link.classList.remove('tabs-link-selected'));
 
     // Add a class named "tabs-link-selected" to this link
-    // this.element;
+    this.element.classList.add('tabs-link-selected');
     
     // Call the select method on the item associated with this link
-
+    this.tabItem.select();
   }
 }
 
 class TabItem {
   constructor(element) {
     // Assign this.element to the passed in element
-    // this.element;
+     this.element = element;
   }
 
   select() {
     // Select all ".tabs-item" elements from the DOM
-    // const items;
+     const items = document.querySelectorAll('.tabs-item');
 
     // Remove the class "tabs-item-selected" from each element
     
