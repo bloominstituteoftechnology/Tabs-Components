9124558d1086fecd04851e211f5efa46f0bfeb01
MichaelNey <ney.quentin.michael@gmail.com>
2018-12-06 14:51:27 -0800

    Carousel now has the ability to go left or right. Automatically goes to the next slide or image every 3 seconds.

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -7,11 +7,13 @@ class Carousel {
         this.slides = this.element.querySelectorAll('img');
         this.nextSlide; //This is an interval that automatically goes to the next slide
         this.activeSlide = document.querySelector('img[data-slide="1"');
+        this.transitioning = false;
         this.initialize();
     }
 
     initialize() {
         this.rightBtn.addEventListener('click', () => this.moveRight());
+        this.leftBtn.addEventListener('click', () => this.moveLeft());
         this.activeSlide.classList.remove('hidden');
         this.nextSlide = setInterval(() => {
             this.moveRight();
@@ -19,22 +21,45 @@ class Carousel {
     }
 
     moveRight() {
-        let currentSlideData = Number(this.activeSlide.dataset.slide);
-        let nextSlideData = currentSlideData + 1;
-        let exitSlide = this.activeSlide;
-        //exitSlide.classList.add('hidden');
-        //If there isn't a slide after the current, loop back to the first
-        if(document.querySelector(`img[data-slide="${nextSlideData}"`) === null) {
-            this.activeSlide = document.querySelector(`img[data-slide="1"`);
-        } else {
-            //If there IS a slider after the current, go to that one.
-            this.activeSlide = document.querySelector(`img[data-slide="${nextSlideData}"`);
+        if(!this.transitioning) {
+            let currentSlideData = Number(this.activeSlide.dataset.slide);
+            let nextSlideData = currentSlideData + 1;
+            let exitSlide = this.activeSlide;
+            //exitSlide.classList.add('hidden');
+            //If there isn't a slide after the current, loop back to the first
+            if(document.querySelector(`img[data-slide="${nextSlideData}"`) === null) {
+                this.activeSlide = document.querySelector(`img[data-slide="1"`);
+            } else {
+                //If there IS a slider after the current, go to that one.
+                this.activeSlide = document.querySelector(`img[data-slide="${nextSlideData}"`);
+            }
+    
+            this.animateCarousel(exitSlide, this.activeSlide);
         }
+        
+    }
 
-        this.animateCarousel(exitSlide, this.activeSlide);
+    moveLeft() {
+        if(!this.transitioning) {
+            let currentSlideData = Number(this.activeSlide.dataset.slide);
+            let nextSlideData = currentSlideData - 1;
+            let exitSlide = this.activeSlide;
+            //exitSlide.classList.add('hidden');
+            //If there isn't a slide before the current, loop to the last
+            if(document.querySelector(`img[data-slide="${nextSlideData}"`)  === null) {
+                this.activeSlide = this.slides[this.slides.length - 1];
+            } else {
+                //If there IS a slider after the current, go to that one.
+                this.activeSlide = document.querySelector(`img[data-slide="$    {nextSlideData}"`);
+            }
+
+            this.animateCarousel(exitSlide, this.activeSlide);
+        }
+        
     }
 
     animateCarousel(curr, next) {
+        this.transitioning = true;
         TweenMax.fromTo(curr, .5, {
             opacity: 1
         }, {
@@ -45,7 +70,10 @@ class Carousel {
                 TweenMax.fromTo(next, .5, {
                     opacity: 0
                 }, {
-                    opacity: 1
+                    opacity: 1,
+                    onComplete: () => {
+                        this.transitioning = false;
+                    }
                 });
             }
         });
