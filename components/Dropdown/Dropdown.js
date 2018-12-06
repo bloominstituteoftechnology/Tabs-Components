b020b6e679e438fbd9147b91715dd98eb1b0b116
Kingaa6 <Kingaa6@gmail.com>
2018-12-06 12:49:03 -0800

    Add menuOpen boolean for tracking

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -1,27 +1,26 @@
 class Dropdown {
   constructor(element) {
 
-    // Assign this.element to the dropdown element
     this.element = element;
-
-    // Get the element with the ".dropdown-button" class found in the dropdown element (look at the HTML for context)
     this.button = this.element.querySelector('.dropdown-button');
-
-    // assign the reference to the ".dropdown-content" class found in the dropdown element
     this.content = this.element.querySelector('.dropdown-hidden');
-
-    // Add a click handler to the button reference and call the toggleContent method.
+    // container is not defined, because it's outside of the dropdown div
+    // container would need to be in a class, then linked to this class
+    // this.container = this.element.querySelector('.container');
+    this.menuOpen = false;
     this.button.addEventListener('click', () => {
       this.toggleContent();
     })
   }
 
   toggleContent() {
-    console.log('clicked');
-    // Toggle the ".dropdown-hidden" class off and on
-    // this.content.classList.toggle('dropdown-hidden');
-    $(this.content).slideToggle('fast');
+    $(this.content).slideToggle('slide');
     this.content.style.display = "flex";
+    if (this.menuOpen) {
+      this.menuOpen = false;
+    } else {
+      this.menuOpen = true;
+    }
   }
 }
 
