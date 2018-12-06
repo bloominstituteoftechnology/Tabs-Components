df566a7af093e4132430c563074de29cc35409c5
bhumip214 <bhumip214@gmail.com>
2018-12-06 17:27:25 -0500

    added two new tabs

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -20,9 +20,7 @@ class TabLink {
     // Get all of the elements with the tabs-link class
     const links = document.querySelectorAll(".tabs-link");
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-    Array.from(links).forEach(link =>
-      link.classList.remove("tabs-link-selected")
-    );
+    links.forEach(link => link.classList.remove("tabs-link-selected"));
     // Add a class named "tabs-link-selected" to this link
     this.element.classList.add("tabs-link-selected");
     // Call the select method on the item associated with this link
