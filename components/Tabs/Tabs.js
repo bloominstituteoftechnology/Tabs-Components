3c175dd594e19adcff1b98b80842db2bb6226917
Olivia Osborn <olioz95@gmail.com>
2018-12-06 16:08:50 -0500

    filled in first few lines of tabs.js file

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -2,14 +2,17 @@
 class TabLink {
   constructor(element) {
     // Assign this.element to the passed in DOM element
-    // this.element;
+    // this.element; 
+    this.element = element
     
     // Get the custom data attribute on the Link
     // this.data;
+    this.data = this.element.dataset.tab;
     
     // Using the custom data attribute get the associated Item element
     // this.itemElement;
-    
+    this.itemElement = document.querySelector(`.tabs-item[data-tab="${this.data}"]`)
+
     // Using the Item element, create a new instance of the TabItem class
     // this.tabItem;
     
@@ -59,4 +62,4 @@ class TabItem {
 
 */
 
-links = document.querySelectorAll();
+let links = document.querySelectorAll(".tabs-link").forEach(link => new TabLink(link));
