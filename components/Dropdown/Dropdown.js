02a26aae326a002a1e9c6d0b8ba62fa3b2c09d57
KieranVieira <kieranvieira@live.com>
2018-12-06 16:09:42 -0500

    Added dropdown menu animation

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -1,3 +1,5 @@
+let dropActive = false;
+
 class Dropdown {
   constructor(element) {
     this.element = element;
@@ -7,9 +9,18 @@ class Dropdown {
       this.toggleContent();
     })
   }
-
   toggleContent() {
-    this.content.classList.toggle('dropdown-hidden');
+    if(!dropActive){
+      // this.content.classList.toggle('dropdown-hidden');
+      dropActive = true;
+      console.log(dropActive);
+      TweenMax.to(".dropdown-content", .5, {top:55})
+    }else if(dropActive){
+      dropActive = false;
+      console.log(dropActive);
+      TweenMax.to(".dropdown-content", .2, {top:-170})
+    }
+
   }
 }
 let dropdowns = document.querySelectorAll('.dropdown').forEach( dropdown => new Dropdown(dropdown));
