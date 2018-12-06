5a43e10f248ce340f36c2ab74a2bcce5967d1259
Kingaa6 <Kingaa6@gmail.com>
2018-12-06 14:47:01 -0800

    Tab content functionality added

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -13,18 +13,14 @@ class TabLink {
   };
 
   select() {
-    console.log('test');
     // Get all of the elements with the tabs-link class
-    // const links = ;
-
+    const links = document.querySelectorAll('.tabs-link');
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-    // Array.from(links).forEach();
-
+    links.forEach(element => element.classList.remove('tabs-link-selected'));
     // Add a class named "tabs-link-selected" to this link
-    // this.element;
-
+    this.element.classList.add('tabs-link-selected');
     // Call the select method on the item associated with this link
-
+    this.tabItem.select();
   }
 }
 
@@ -35,12 +31,11 @@ class TabItem {
 
   select() {
     // Select all ".tabs-item" elements from the DOM
-    // const items = ;
-
+    const items = document.querySelectorAll('.tabs-item');
     // Remove the class "tabs-item-selected" from each element
-
-    // Add a class named "tabs-item-selected" to this element
-    //this.element;
+    items.forEach(param => param.classList.remove('tabs-item-selected'));
+    // Add a class named "tabs-link-selected" to this link;
+    this.element.classList.add('tabs-item-selected');
   }
 }
 
