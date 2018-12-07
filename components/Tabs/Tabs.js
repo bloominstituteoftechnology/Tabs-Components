92fa18a24c4a1f249329ec22a89a8ae20502b4ff
linkel <ironcarbide@gmail.com>
2018-12-06 16:16:08 -0800

    Stretch goal for tabs.

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -1,4 +1,14 @@
 
+class Tabs {
+  constructor(element) {
+    this.element = element;
+    // grab the tabs-link nodes
+    this.links = this.element.querySelectorAll(".tabs-link");
+    // Make a new TabLink for each node
+    this.links = this.links.forEach( link => new TabLink(link))
+  }
+}
+
 class TabLink {
   constructor(element) {
     // Assign this.element to the passed in DOM element
@@ -14,22 +24,31 @@ class TabLink {
     this.tabItem = new TabItem(this.itemElement);
     
     // Add a click event listener on this instance, calling the select method on click
-    this.element.addEventListener("click", () => {this.select()});
+    this.element.addEventListener("click", () => {
+      this.select();
+      this.deselect();
+    });
   };
 
   select() {
+    // Call the select method on the item associated with this link
+    this.tabItem.deselect();
+    this.tabItem.select();
+
+  }
+
+  deselect() {
     // Get all of the elements with the tabs-link class
     //let links = document.querySelector(".tabs-link"); redeclaration breaks the functionality?
 
     Array.from(links).forEach( item => item.classList.remove("tabs-link-selected"));
-    
+
     // Add a class named "tabs-link-selected" to this link
     this.element.classList.add("tabs-link-selected");
-    
-    // Call the select method on the item associated with this link
-    this.tabItem.select();
   }
 }
+
+
 
 class TabItem {
   constructor(element) {
@@ -38,13 +57,17 @@ class TabItem {
   }
 
   select() {
+
+    // Add a class named "tabs-item-selected" to this element
+    this.element.classList.add("tabs-item-selected");
+  }
+
+  deselect() {
     // Select all ".tabs-item" elements from the DOM
     const items = document.querySelectorAll(".tabs-item");
 
     // Remove the class "tabs-item-selected" from each element
     Array.from(items).forEach( item => item.classList.remove("tabs-item-selected"));
-    // Add a class named "tabs-item-selected" to this element
-    this.element.classList.add("tabs-item-selected");
   }
 }
 
@@ -58,6 +81,12 @@ class TabItem {
 
 */
 
+// This variable links is currently necessarily to enable functionality on deselect for TabLink. Ideally should be
+// written so that this links variable is not here, and the functionality is done by TabLink entirely.
+
 let links = document.querySelectorAll(".tabs-link");
 
-links.forEach( link => new TabLink(link))
+//let tab_components = document.querySelectorAll(".tabs")
+let tab_component = document.querySelector(".tabs");
+
+firstTab = new Tabs(tab_component);
