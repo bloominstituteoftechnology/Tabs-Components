6e23efb1f3c4142692a882a894c79e56ece78632
Kelli Rae Hanson <kelliraehanson@gmail.com>
2018-12-13 17:54:21 -0600

    commit before nightly stand up

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -5,13 +5,13 @@ class TabLink {
     this.element = element;
     
     // Get the custom data attribute on the Link
-    this.data;
+    this.data = this.element.dataset.tab;
     
     // Using the custom data attribute get the associated Item element
-    this.itemElement;
+    this.itemElement = document.querySelector(`.content[data-tab = '${this.data}']`);//not finished
     
     // Using the Item element, create a new instance of the TabItem class
-    this.tabItem;
+    this.tabItem = new TabItem(this.elementsomething);//not finished
     
     // Add a click event listener on this instance, calling the select method on click
 
@@ -19,7 +19,7 @@ class TabLink {
 
   select() {
     // Get all of the elements with the tabs-link class
-    const links;
+    const links = document.querySelectorAll(".tabs-link"); //not finished
 
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
     Array.from(links).forEach();
@@ -40,7 +40,7 @@ class TabItem {
 
   select() {
     // Select all ".tabs-item" elements from the DOM
-    const items;
+    const items = document.querySelectorAll(".tabs-item"); //not finished
 
     // Remove the class "tabs-item-selected" from each element
     
