1de912560ff0532750d46e92eacdf330019955d3
JamesB <jblazer1@yahoo.com>
2018-12-13 13:58:01 -0800

    Dropdown Menu working

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -2,19 +2,19 @@
 class TabLink {
   constructor(element) {
     // Assign this.element to the passed in DOM element
-    // this.element;
+    // this.element = element;
     
     // Get the custom data attribute on the Link
-    // this.data;
+    // this.data = this.element.dataset.tab;
     
     // Using the custom data attribute get the associated Item element
-    // this.itemElement;
+    // this.itemElement = document.querySelector(`.tabs-item[data-tab="${this.data}"]`);
     
     // Using the Item element, create a new instance of the TabItem class
-    // this.tabItem;
+    // this.tabItem = new TabItem(this.tabItem);
     
     // Add a click event listener on this instance, calling the select method on click
-
+    this.tabItem.addEventListener('click', () => {this.select()});
   };
 
   select() {
@@ -59,4 +59,4 @@ class TabItem {
 
 */
 
-links = document.querySelectorAll();
+links = document.querySelectorAll('.tabs-link').forEach(link => new TabLink(link));
