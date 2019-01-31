aad2dfa20963b2ca77495447431c73230af76ad2
omar salah <42569856+OmarSalah95@users.noreply.github.com>
2019-01-31 15:05:26 -0800

    Had some tweenmax animation issues since solved on the drop down.

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -5,9 +5,12 @@ class Dropdown {
     this.content = this.element.querySelector('.dropdown-content');
     this.button.addEventListener('click', () => this.toggleContent())
   }
-
+  
   toggleContent() {
     this.content.classList.toggle('dropdown-hidden');
+    !this.content.classList.contains('dropdown-hidden')
+    ?TweenMax.to( this.content , 1, { y:425, rotation: 360} )
+    :TweenMax.to( this.content , 2, { y:-425, rotation: -360} )
   }
 }
 
