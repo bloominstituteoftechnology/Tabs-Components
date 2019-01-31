c1e1fbae036b0e481590c93580329a889b4b370f
omar salah <42569856+OmarSalah95@users.noreply.github.com>
2019-01-31 13:16:12 -0800

    MVP is now complete.

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -1,4 +1,3 @@
-
 class TabLink {
   constructor(element) {
     this.element = element;
@@ -6,10 +5,10 @@ class TabLink {
     this.itemElement = document.querySelector(`.tabs-item[data-tab='${this.data}']`);
     this.tabItem = new TabItem(this.itemElement)
     this.element.addEventListener("click", e => this.select(e));
-  };
-
+  }
   select() {
-    const links = document.querySelectorAll('.tabs-link').forEach(link => link.classList.remove('tabs-link-selected'));
+    const links = document.querySelectorAll('.tabs-link');
+    links.forEach(link => link.classList.remove('tabs-link-selected'));
     this.element.classList.add('tabs-link-selected')
     this.tabItem.select()
   }
@@ -19,12 +18,10 @@ class TabItem {
   constructor(element) {
     this.element = element;
   }
-
   select() {
-    const items = document.querySelectorAll('.tabs-item').forEach(item => item.classList.remove('tabs-item-selected'))
-
+    const items = document.querySelectorAll('.tabs-item').forEach(item =>item.classList.remove("tabs-item-selected"));
+    this.element.classList.add("tabs-item-selected");
   }
 }
-
 
 let links = document.querySelectorAll('.tabs-link').forEach( link => new TabLink(link));
