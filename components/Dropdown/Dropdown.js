f4ca1d71114798365f676094843e46c666e4c409
omar salah <42569856+OmarSalah95@users.noreply.github.com>
2019-01-31 17:51:01 -0800

    Debugged the single fire issue on the animations for the tab items.

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -9,8 +9,8 @@ class Dropdown {
   toggleContent() {
     this.content.classList.toggle('dropdown-hidden');
     !this.content.classList.contains('dropdown-hidden')
-    ?TweenMax.to( this.content , 1, { y:425, rotation: 360} )
-    :TweenMax.to( this.content , 2, { y:-425, rotation: -360} )
+      ?TweenMax.to( this.content , 1, { y:425, rotation: 360} )
+      :TweenMax.to( this.content , 2, { y:-425, rotation: -360} )
   }
 }
 
--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -4,13 +4,14 @@ class TabLink {
     this.data = this.element.dataset.tab;
     this.itemElement = document.querySelector(`.tabs-item[data-tab='${this.data}']`);
     this.tabItem = new TabItem(this.itemElement)
-    this.element.addEventListener("click", e => this.select(e));
+    this.element.addEventListener("click", e => this.select());
   }
   select() {
     const links = document.querySelectorAll('.tabs-link');
     links.forEach(link => link.classList.remove('tabs-link-selected'));
     this.element.classList.add('tabs-link-selected')
     this.tabItem.select()
+    console.log(this.itemElement); 
   }
 }
 
@@ -19,10 +20,10 @@ class TabItem {
     this.element = element;
   }
   select() {
-    const items = document.querySelectorAll('.tabs-item').forEach(item =>item.classList.remove("tabs-item-selected"));
+    const items = document.querySelectorAll('.tabs-item').forEach(item => {item.classList.remove("tabs-item-selected"), TweenMax.to( this.element , 0, {opacity:0} )});
     this.element.classList.add("tabs-item-selected")
-    TweenMax.to( this.element , 3, { opacity:1} )
+    TweenMax.to( this.element , 4, {opacity:1} )
   }
-}
+}   
 
 let links = document.querySelectorAll('.tabs-link').forEach( link => new TabLink(link));
