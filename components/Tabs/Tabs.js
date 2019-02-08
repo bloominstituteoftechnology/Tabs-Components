00d031db4eace955ab714c3796270e917955c2e9
Wesley White <wwhite@Wesleys-MacBook-Pro.local>
2019-02-07 17:58:35 -0800

    finshed TabLink class, working on select()

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -10,18 +10,19 @@ class TabLink {
     
     // Using the custom data attribute get the associated Item element
     // this.itemElement;
-    this.elementContent = document.querySelector(`.tabs-items[data-tab="${this.elementData}"]`);
+    this.itemElement = document.querySelector(`.tabs-items[data-tab="${this.elementData}"]`);
     
     // Using the Item element, create a new instance of the TabItem class
     // this.tabItem;
-    
+    this.tabItem = new TabItem(this.itemElement);
     // Add a click event listener on this instance, calling the select method on click
-
+    this.link.addEventListener('click', element => this.select())
   };
 
   select() {
     // Get all of the elements with the tabs-link class
-    // const links;
+    const links= links.querySelectorAll('.tabs-link')
+
 
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
     // Array.from(links).forEach();
