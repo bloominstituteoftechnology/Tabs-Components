e82bc556f7cc567357a8664425a5d779dff2e65c
Ashley <jal9626@gmail.com>
2019-02-07 22:34:07 -0500

    Complex Component Building

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -3,13 +3,13 @@ class Dropdown {
     
     // Assign this.element to the dropdown element
     this.element = element;
-    
+    console.log(this.element);
     // Get the element with the ".dropdown-button" class found in the dropdown element (look at the HTML for context)
     this.button = this.element.querySelector('.dropdown-button');
-    
+    console.log(this.button);
     // assign the reference to the ".dropdown-content" class found in the dropdown element
     this.content = this.element.querySelector('.dropdown-content');
-    
+    console.log(this.content);
     // Add a click handler to the button reference and call the toggleContent method.
     this.button.addEventListener('click', () => {
         this.toggleContent();
@@ -19,7 +19,7 @@ class Dropdown {
   toggleContent() {
     
     // Toggle the ".dropdown-hidden" class off and on
-    this.content.classList.toggle('.dropdown-hidden');
+    this.content.classList.toggle('dropdown-hidden');
   }
 }
 
