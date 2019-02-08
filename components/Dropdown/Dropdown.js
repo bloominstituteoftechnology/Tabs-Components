8ad1216b50b65230e3ebe2b5bb2698c5809955d7
Wesley White <wwhite@Wesleys-MacBook-Pro.local>
2019-02-07 16:02:00 -0800

    fixed dropdown with toggle

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -12,14 +12,14 @@ class Dropdown {
     
     // Add a click handler to the button reference and call the toggleContent method.
     this.button.addEventListener('click', () => {
-      this.toggleContent()
+     this.toggleContent()
     })
   }
 
   toggleContent() {
     
     // Toggle the ".dropdown-hidden" class off and on
-    this.content.classList.toggle(".dropdown-hidden")
+    this.content.classList.toggle("dropdown-hidden")
   }
 }
 
