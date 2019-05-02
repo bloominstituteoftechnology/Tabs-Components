1f7474699298889600720fbe10b6f463f555fe86
JustSpokenCodes <JustSpokenCodes@users.noreply.github.com>
2019-05-01 20:50:16 -0700

    ok then..

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -2,10 +2,10 @@ class Dropdown {
   constructor(element) {
     
     // Assign this.element to the dropdown element
-    this.element;
+    this.element = document.querySelectorAll('.dropdown');
     
     // Get the element with the ".dropdown-button" class found in the dropdown element (look at the HTML for context)
-    this.button = this.element.querySelector();
+    this.button = this.element.querySelector('');
     
     // assign the reference to the ".dropdown-content" class found in the dropdown element
     this.content;
