c21b5e608cee0eb498b55897c063e19d302462f3
JustSpokenCodes <JustSpokenCodes@users.noreply.github.com>
2019-05-02 18:13:21 -0700

    lets see if i can do it right this time

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -1,4 +1,3 @@
-
 class TabLink {
   constructor(element) {
     // Assign this.element to the passed in DOM element
@@ -50,13 +49,9 @@ class TabItem {
 }
 
 /* START HERE: 
-
 - Select all classes named ".tabs-link" and assign that value to the links variable
-
 - With your selection in place, now chain a .forEach() method onto the links variable to iterate over the DOM NodeList
-
 - In your .forEach() method's callback function, return a new instance of TabLink and pass in each link as a parameter
-
 */
 
 links = document.querySelectorAll();
