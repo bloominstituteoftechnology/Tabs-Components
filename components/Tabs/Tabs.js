71b251bc7066271da9b86a351b5092fa5eb03273
teej387 <teej387@yahoo.com>
2019-05-09 20:34:04 -0500

    created click event that triggers select() method on tabs

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -6,27 +6,31 @@ class TabLink {
     // console.log(this.element)
     // Get the custom data attribute on the Link
     this.data = document.querySelector(`.tabs-link[data-tab='${this.element.dataset.tab}']`);
-    console.log(this.data)
+    // console.log(this.data)
     
     // Using the custom data attribute get the associated Item element
-    // this.itemElement;
-    
+    this.itemElement = document.querySelector(`.tabs-item[data-tab='${this.element.dataset.tab}']`);
+    // console.log(this.itemElement)
+
     // Using the Item element, create a new instance of the TabItem class
-    // this.tabItem;
+    this.tabItem = new TabItem(this.tabItem);
     
     // Add a click event listener on this instance, calling the select method on click
-
+    this.element.addEventListener('click', () => this.select());
+    
   };
 
   select() {
     // Get all of the elements with the tabs-link class
-    // const links;
-
+    const links = document.querySelectorAll('.tabs-link');
+    
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-    // Array.from(links).forEach();
-
+    Array.from(links).forEach(link => {
+      link.classList.remove('tabs-link-selected');
+    });
+  
     // Add a class named "tabs-link-selected" to this link
-    // this.element;
+    this.element.classList.add('tabs-link-selected');
     
     // Call the select method on the item associated with this link
 
