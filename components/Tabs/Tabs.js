37d66b1135363ed3ed5c8251eb39d22ae6edb230
bipolarbearstrikesback <49734722+bipolarbearstrikesback@users.noreply.github.com>
2019-05-16 15:16:00 -0500

    Update Tabs.js

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -20,14 +20,19 @@ class TabLink {
   select() {
     // Get all of the elements with the tabs-link class
      const links = documents.querySelector("tabs-link");
+    Array.from(links).forEach(link => {
+      link.classList.remove("tabs-link-selected");
+    });
+    this.link.classList.add("tabs-link-selected")
 
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-    // Array.from(links).forEach();
+     //Array.from(links).forEach();
 
     // Add a class named "tabs-link-selected" to this link
     // this.element;
     
     // Call the select method on the item associated with this link
+        this.tabItem.select();
 
   }
 }
@@ -35,7 +40,7 @@ class TabLink {
 class TabItem {
   constructor(element) {
     // Assign this.element to the passed in element
-    // this.element;
+    this.element=element;
   }
 
   select() {
