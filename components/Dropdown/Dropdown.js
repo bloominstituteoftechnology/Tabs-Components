b0beb01a8e2b197b23656f37687106f007af4b96
Carl Stanley <49922081+Carl-Stanley@users.noreply.github.com>
2019-06-13 18:43:07 -0700

    Commit

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -2,13 +2,17 @@ class Dropdown {
   constructor(element) {
     
     // Assign this.element to the dropdown element
-    this.element;
+    this.element = element;
     
     // Get the element with the ".dropdown-button" class found in the dropdown element (look at the HTML for context)
+<<<<<<< HEAD
     //this.button = this.element.querySelector();
+=======
+    this.button = this.element.querySelector('.dropdown-button');
+>>>>>>> 139a987f48b5588d8fd02ec27403ec53e944e825
     
     // assign the reference to the ".dropdown-content" class found in the dropdown element
-    this.content;
+    this.content = document.querySelector('.dropdown-content');
     
     // Add a click handler to the button reference and call the toggleContent method.
     this.button.addEventListener('click', () => {
