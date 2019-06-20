de43cc0f7e825503795804462e9c4d641b4cb5c8
Greg Johnston <johng3269@gmail.com>
2019-06-20 12:26:53 -0400

    Project done

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -23,13 +23,13 @@ class TabLink {
     const links = document.querySelectorAll('.tabs-link');
 
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-    Array.from(links).forEach((link) => link.classList.remove('tabs-link-selected'));
-    
+    Array.from(links).forEach((link) => {link.classList.remove('tabs-link-selected')});
+
     // Add a class named "tabs-link-selected" to this link
-    // this.element;
+    this.element.classList.add('tabs-link-selected');
 
     // Call the select method on the item associated with this link
-
+    this.tabItem.select();
   }
 }
 
@@ -41,12 +41,11 @@ class TabItem {
 
   select() {
     // Select all ".tabs-item" elements from the DOM
-    const items = document.querySelectorAll('.tab-items')
-
+    const items = document.querySelectorAll('.tabs-item')
     // Remove the class "tabs-item-selected" from each element
-
+    Array.from(items).forEach((item) => {item.classList.remove('tabs-item-selected')});
     // Add a class named "tabs-item-selected" to this element
-    //this.element;
+    this.element.classList.add('tabs-item-selected');
   }
 }
 
